c0171a4987c50862cda2bb12b2dd2dc989eece52
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-08 23:40:26 -0600

    format what should be most of main file with API check call

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
+import './styles/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/index.css'
-import { getAPIStatusRequest } from './api/GetApiStatusRequest.ts'
+
+import { getApiStatusRequest } from './api' // status check message for connection to API
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App apiStatus={await getAPIStatusRequest()} />
+    <App apiStatus={await getApiStatusRequest()} />
   </React.StrictMode>,
 )
