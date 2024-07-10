844d64d2e1685953805d3ea723d2fa936a80c79f
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-09 22:54:21 -0600

    add lins infobox under sketch

---
+++ src/components/_RelatedLinksInfobox.tsx
@@ -0,0 +1,23 @@
+import { IGetAPIStatusResponse } from "../interfaces";
+
+export interface IRelatedLinksInfoboxProps {
+  apiStatus: IGetAPIStatusResponse;
+}
+
+export function RelatedLinksInfobox({ apiStatus }: IRelatedLinksInfoboxProps) {
+  const githubUrl = "https://github.com/horaciovelvetine";
+  const frontendUrl = "https://github.com/horaciovelvetine";
+  const backendUrl = "https://github.com/horaciovelvetine";
+
+  return (
+    <>
+      <div id="related-links-container">
+        <ul id="related-links-list">
+          <li id="related-link">Github: <a href={frontendUrl}>Frontend</a> | <a href={backendUrl}>Backend</a></li>
+          <li id="related-link">©2024 by <a href={githubUrl}>@horaciovelvetine</a></li>
+          <li id="status-link">API Status: <i>{apiStatus.status}</i> {apiStatus.message}</li>
+        </ul>
+      </div>
+    </>
+  );
+};
