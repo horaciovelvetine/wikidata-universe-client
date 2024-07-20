07814d8208eb6fe60ffda419ca8e44b87ec63a69
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-20 13:08:56 +0100

    fix errant class naming

--- src/functions/_DynamicDisplayStateMap.ts
+++ src/functions/_DynamicDisplayStateMap.ts
@@ -4,6 +4,6 @@ export const DynamicDisplayStateMap = {
   [DISPLAY_STATES.HIDDEN]: "display-hidden",
   [DISPLAY_STATES.LOADING]: "display-loading",
   [DISPLAY_STATES.ERROR]: "display-error",
-  [DISPLAY_STATES.SHOWN]: "show-init-query",
+  [DISPLAY_STATES.SHOWN]: "display-shown",
   [DISPLAY_STATES.REMOVED]: "display-removed",
 };
