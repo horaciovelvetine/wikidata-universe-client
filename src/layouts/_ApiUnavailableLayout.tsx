a773c83c96e9ca299a5efff680bc97eb9ad451f8
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-04 22:10:25 -0600

    init basic error hold screen for when api unavailable

--- src/layouts/_ApiUnavailableLayout.tsx
+++ src/layouts/_ApiUnavailableLayout.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import '../assets/styles/ApiUnavailableLayout.css'
+
+import { ErrorSketch } from '../components';
+import React, { memo, useEffect } from 'react';
+import { fadeInElement } from '../functions';
+
+const MemoizedSketch = memo(ErrorSketch);
 
 const ApiUnavailableLayout: React.FC = () => {
+  const containerRef = React.createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    setTimeout(() => {
+      fadeInElement(containerRef.current!);
+    }, 1);
+  }, []);
+
   return (
-    <div>
-      <h1>API Unavailable</h1>
-      <p>Sorry, the API is currently unavailable. Please try again later.</p>
-    </div>
+    <>
+      <div id='error-message-container' ref={containerRef}>
+        <h1>API Unavailable</h1>
+        <p>Sorry, the API is currently unavailable. Please try again later.</p>
+      </div>
+      <div id='sketch-layout-container'>
+        <MemoizedSketch />
+      </div>
+    </>
   );
 };
 
