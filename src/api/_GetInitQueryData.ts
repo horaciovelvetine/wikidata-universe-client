d26ef323aade60618ac14c24f656e8ec00658f94
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-06 22:51:02 -0600

    inits working initial request, gets details on initial ent

---
+++ src/api/_GetInitQueryData.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { buildApiUrl } from "../functions";
+import { IApiStatus, IWikidataUniverseSession } from "../interfaces";
+
+interface GetInitQueryDataRequestProps {
+  queryVal: string;
+}
+
+interface GetInitQueryDataResponse {
+  status: IApiStatus;
+  data: IWikidataUniverseSession;
+}
+
+
+export async function getInitQueryDataRequest({ queryVal }: GetInitQueryDataRequestProps): Promise<GetInitQueryDataResponse> {
+  return await axios.get(buildApiUrl("init-query-data"), {
+    params: {
+      query: queryVal
+    }
+  }).then(res => {
+    return { status: { code: 200, message: "OK" }, data: res.data };
+  }).catch(err => {
+    console.error(err);
+    return {
+      status: { code: 500, message: "Internal Server Error" }, data: {} as IWikidataUniverseSession
+    }
+  });
+}
