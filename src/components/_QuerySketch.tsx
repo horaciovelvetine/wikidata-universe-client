9f6030a0495fa8ff8f50f2d0706fca460f277e62
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-12 20:35:44 -0600

    implements displaying one vertex, clicking said vertex successfully identifying itself
    adds a bit of togleable UI elements for debugging and general use, with toggle params
    
    working as min expected for a single entity.

--- src/components/_QuerySketch.tsx
+++ src/components/_QuerySketch.tsx
@@ -1,93 +1,87 @@
 import '../styles/components/P5SketchMain.css';
 import { ReactP5Wrapper, P5CanvasInstance, Sketch } from '@p5-wrapper/react';
-import { IPoint3D, ISessionData } from '../interfaces';
+import { ISessionData } from '../interfaces';
 import DemoTestSessionBody from '../assets/data/session-body-r1-1.json'
 import { Camera } from 'p5';
+import { Vertex } from '../_p5';
+import { drawAxisGiz, drawBoundBoxGiz, drawConstants } from '../_p5/UI';
+import { traceRay } from '../functions';
 
 interface QuerySketchProps {
   session: ISessionData;
 }
 
-let cxCam: Camera;
-let origCam: Camera;
+let cam: Camera;
+// let obsCam: Camera
+
+let params = {
+  displayAxis: true,
+  displayBoundingBox: true,
+}
 
 export const QuerySketch: React.FC<QuerySketchProps> = ({ session }) => {
-  const sketch: Sketch = (p5: P5CanvasInstance) => {
+  const sketch: Sketch = (p5) => {
+    //SETUP
     p5.setup = () => {
       p5.createCanvas(session.dimensions.width, session.dimensions.height, p5.WEBGL);
-      cxCam = p5.createCamera();
-      origCam = p5.createCamera();
-      p5.setCamera(cxCam)
+      cam = p5.createCamera();
+      cam.setPosition(0, 0, 100);
+      cam.lookAt(0, 0, 0);
     }
+    // DRAWING
     p5.draw = () => {
-      p5.background(255, 255, 255);
-      p5.orbitControl();
-      p5.lights();
-      // p5.fill(255,255,255,17)
-      // p5.box(p5.width, p5.height, p5.width)
-      // p5.normalMaterial();
+      drawConstants(p5)
+      // move near plane closer
+      const aspRatio = p5.width / p5.height
+      const defFov = (2 * p5.atan(p5.height / 2 / 800));
+      p5.perspective(defFov, aspRatio, 1)
+
+      // TOGGLES
+      if (params.displayBoundingBox) {
+        drawBoundBoxGiz(p5);
+      }
+      if (params.displayAxis) {
+        drawAxisGiz(p5);
+      }
+
       DemoTestSessionBody.vertices.forEach(v => {
-        let tsVert = new Vertex(v.label, v.description, v.coords);
-        // p5.translate(tsVert.coordinates.x, tsVert.coordinates.y, tsVert.coordinates.z);
-        tsVert.draw(p5);
+        new Vertex(v.label, v.description, v.coords).draw(p5);
       })
+
     }
 
-    p5.mousePressed = (e: MouseEvent) => {
-      // add a magnitude mult
-      // const x = 100 * (p5.mouseX - p5.width / 2) / (p5.width / 2);
-      // const y = 100 * (p5.mouseY - p5.height / 2) / (p5.height / 2);
-      // console.log({ pX: p5.mouseX, pY: p5.mouseY, eX: e.clientX, eY: e.clientY, width: p5.width, height: p5.height })
-      // console.log({ pXAdjust: x, pYAdjust: y })
-      console.log(cxCam);
-      // DemoTestSessionBody.vertices.forEach(v => {
-      //   if (p5.dist(x, y, v.coords.x, v.coords.y) < 5) {
-      //     console.log('Vertex clicked:', v.label);
-      //   }
-      // })
+    //HANDLING
+    p5.keyPressed = (e: KeyboardEvent) => {
+      // print info to console
+      if (e.key == "i") {
+        console.log({ cam })
+      }
+
+      if (e.key == "a") {
+        params.displayAxis = !params.displayAxis
+      }
+
+      if (e.key == "b") {
+        params.displayBoundingBox = !params.displayBoundingBox
+      }
+
+      if (e.key == "c") {
+        console.log('cam switch impl!')
+      }
+    }
+
+    p5.mouseClicked = () => {
+      DemoTestSessionBody.vertices.forEach(vertData => {
+        const v = new Vertex(vertData.label, vertData.description, vertData.coords)
+        const isVertSelection = traceRay(p5, cam, v);
+        if (isVertSelection) {
+          console.log(v.label + " clicked.")
+        } else {
+          console.log("no target clicked.")
+        }
+      })
     }
   }
 
   return <ReactP5Wrapper sketch={sketch} />
-}
-
-class Vertex {
-  label: string;
-  description: string;
-  coordinates: IPoint3D;
-
-  constructor(label: string, description: string, coordinates: IPoint3D) {
-    this.label = label;
-    this.description = description;
-    this.coordinates = coordinates;
-  }
-
-  draw(p5: P5CanvasInstance) {
-    p5.push();
-    p5.translate(this.coordinates.x, this.coordinates.y, this.coordinates.z);
-    p5.color(255, 255, 255, 0.0)
-    p5.strokeWeight(1.5);
-    p5.fill(245, 245, 245, 65)
-    p5.box(10)
-    p5.pop();
-  }
-}
-
-class Edge {
-  srcId: string;
-  tgtId: string;
-  label: string;
-
-  constructor(srcId: string, tgtId: string, label: string) {
-    this.srcId = srcId;
-    this.tgtId = tgtId;
-    this.label = label;
-  }
-
-  draw(p5: P5CanvasInstance) { }
-}
-
-
-function drawBoundingBox(p5: P5CanvasInstance) {
-
 }
