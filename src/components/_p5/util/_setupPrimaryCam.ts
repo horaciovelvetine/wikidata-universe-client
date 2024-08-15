1bdd50e5b4c06f942883cdaffc50fa95a0a2e565
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-14 20:04:48 -0600

    return cams to dynamically setup obs cam, ensures not undefined on hoist-y

--- src/components/_p5/util/_setupPrimaryCam.ts
+++ src/components/_p5/util/_setupPrimaryCam.ts
@@ -1,8 +1,9 @@
 import { P5CanvasInstance, SketchProps } from "@p5-wrapper/react";
 import { Camera } from "p5";
 
-export function setupPrimaryCam(cam: Camera, p5: P5CanvasInstance<SketchProps>) {
+export function setupPrimaryCam(cam: Camera, p5: P5CanvasInstance<SketchProps>): Camera {
   cam = p5.createCamera();
   cam.setPosition(0, 0, 100);
   cam.lookAt(0, 0, 0);
+  return cam;
 }
