fa1536958f8848e7ef0bbabaa2db6aee08913e1d
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-15 20:50:22 -0600

    removes lights, realizes now there is no light in space and need some time for self brb

--- src/components/_p5/UI/_drawUIConstants.ts
+++ src/components/_p5/UI/_drawUIConstants.ts
@@ -3,7 +3,6 @@ import { P5CanvasInstance } from "@p5-wrapper/react";
 export function drawUIConstants(p5: P5CanvasInstance) {
   p5.background(1, 1, 14);
   p5.orbitControl(2, 2, 2);
-  p5.lights();
   p5.rectMode(p5.CENTER);
   // Moves the near plane closer to the camera, otherwise default values
   const aspRatio = p5.width / p5.height
