67a9d8a1a40861e4c999e647da13474fe8718156
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-17 10:10:07 -0600

    init get request to ping api check for awakeness

---
+++ src/api/_GetApiStatus.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { ApiStatus } from "../interfaces";
+import { API_URL } from "./_ApiUrl";
+
+export async function getApiStatus(): Promise<ApiStatus> {
+  return await axios
+    .get(API_URL("status"))
+    .then((res) => {
+      return { code: res.status, message: res.data };
+    }).catch(err => {
+      if (err.code === "ERR_NETWORK") {
+        return { message: "The WikiData Universe API is currently offline. Try again later.", code: 500 };
+      }
+      debugger;
+      return { message: "Unknown Error Encountered.", code: 404 };
+    });
+}
