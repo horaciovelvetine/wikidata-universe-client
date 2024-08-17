7fb02a9988d2556368fdbaee03c5a692a26950eb
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-17 11:17:28 -0600

    init primary app call and pass api status

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@
 // import './styles/Fonts.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { WikiverseApp } from './app/WikiverseApp'
+import { getApiStatus } from './api'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <RootLayout apiStatus={await getApiStatusRequest()} /> */}
+    <WikiverseApp apiStatusRes={await getApiStatus()} />
   </React.StrictMode>
 )
