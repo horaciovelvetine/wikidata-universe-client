78cb1f9297aea18c104e86dd4192e00f6987c72c
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-17 12:44:29 -0600

    adds preloaded and setup features for fonts and cameras
    adds drawing of incoming props sessiondata

--- src/p5/WikiverseSketch.tsx
+++ src/p5/WikiverseSketch.tsx
@@ -1,26 +1,36 @@
+
 import { P5CanvasInstance, ReactP5Wrapper, Sketch } from "@p5-wrapper/react";
-import { calcSafeDimensions } from "./functions";
+import { calcSafeDimensions, setupCameraView } from "./functions";
 import { SessionData } from "../interfaces";
 import { drawSketchConsts } from "./UI";
+import { Camera, Font } from "p5";
+import { Vertex } from "./models";
+import CharisBold from "../assets/font/CharisSIL-Bold.ttf";
 
 interface WikiverseSketchProps {
   session: SessionData;
 }
 
-export const WikiverseSketch: React.FC<WikiverseSketchProps> = () => {
-  //*
-  //* Define the P5 sketch
-  //*
-  const WikiverseSketch: Sketch = (p5: P5CanvasInstance) => {
-    p5.preload = () => { };
+export const WikiverseSketch: React.FC<WikiverseSketchProps> = ({ session }) => {
+  let cam: Camera;
+  let hoveredVertex: Vertex | undefined;
+  let font: Font;
 
+  const WikiverseSketch: Sketch = (p5: P5CanvasInstance) => {
+    p5.preload = () => { font = p5.loadFont(CharisBold); };
     p5.setup = () => {
       const { width, height } = calcSafeDimensions();
       p5.createCanvas(width, height, p5.WEBGL);
+      p5.textFont(font);
+      cam = setupCameraView(p5, cam);
     };
 
     p5.draw = () => {
       drawSketchConsts(p5);
+      session.vertices.forEach((vertex) => {
+        new Vertex(vertex).draw(p5);
+      });
+
     };
 
     p5.mouseClicked = () => { };
