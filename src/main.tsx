46ed5c3c8c6235257b98644b04c5e19f1510e60d
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-17 09:33:29 -0600

    move attempt 1 to side, get working blank canvas

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
-import './styles/Global.css'
-import './styles/Fonts.css'
+// import './styles/Global.css'
+// import './styles/Fonts.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RootLayout } from './layouts/_Root'
-import { getApiStatusRequest } from './api' // status check needed API's
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RootLayout apiStatus={await getApiStatusRequest()} />
+    {/* <RootLayout apiStatus={await getApiStatusRequest()} /> */}
   </React.StrictMode>
 )
