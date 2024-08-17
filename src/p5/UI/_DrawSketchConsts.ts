b8432b43a2ba21a334507e43333562f34abe6a19
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-17 12:03:48 -0600

    init simple drawConsts and export for background lock, orbit control & pov adjustments

---
+++ src/p5/UI/_DrawSketchConsts.ts
@@ -0,0 +1,11 @@
+import { P5CanvasInstance } from "@p5-wrapper/react";
+
+export function drawSketchConsts(p5: P5CanvasInstance) {
+  p5.background('rgb(1, 1, 14)');
+  p5.orbitControl(2, 2, 2);
+  p5.rectMode(p5.CENTER);
+  // move near plane closer to camer, otherwise default
+  const aspectRatio = p5.width / p5.height;
+  const defaultFoc = (2 * p5.atan(p5.height / 2 / 800));
+  p5.perspective(defaultFoc, aspectRatio, 1);
+}
---
+++ src/p5/UI/index.ts
@@ -0,0 +1,1 @@
+export { drawSketchConsts } from './_DrawSketchConsts';
