f5d25c99cdb080bece8d96f5b5753a46083f83fe
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-17 12:43:51 -0600

    init sketch first approach

--- src/app/WikiverseApp.tsx
+++ src/app/WikiverseApp.tsx
@@ -1,19 +1,32 @@
 import './WikiverseAppStyle.css';
-import React from 'react';
-import { ApiStatus } from '../interfaces';
-import { WikiverseSketch } from '../p5/WikiverseSketch';
+import React, { useState } from 'react';
+import { ApiStatus, SessionData } from '../interfaces';
 import { VerticalSiteTitle, Footer } from '../components';
+import { Vertex } from '../p5/models';
+import { WikiverseSketch } from '../p5/WikiverseSketch';
+
+import SessionDataMock from '../assets/data/session-body-r1-1.json';
 
 interface WikiverseAppProps {
   apiStatusRes: ApiStatus;
 }
 
+const res1: SessionData = {
+  query: 'Kevin Bacon',
+  vertices: SessionDataMock.vertices,
+  edges: SessionDataMock.edges,
+  properties: SessionDataMock.properties,
+  queue: SessionDataMock.queue,
+};
+
 export const WikiverseApp: React.FC<WikiverseAppProps> = ({ apiStatusRes }) => {
+  const [query, setQuery] = useState<string>(res1.query!);
+  const [curSelVertex, setCurSelVertex] = useState<Vertex>();
 
   return (
     <>
       <VerticalSiteTitle />
-      <WikiverseSketch session={{ query: undefined }} />
+      <WikiverseSketch session={res1} />
       <Footer />
     </>
   );
