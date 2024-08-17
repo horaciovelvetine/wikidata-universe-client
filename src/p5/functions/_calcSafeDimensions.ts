2153342f3cbf368856673c5f461d085c7afdc516
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-17 11:56:01 -0600

    add dimension calc in p5 where it actually gets used

---
+++ src/p5/functions/_calcSafeDimensions.ts
@@ -0,0 +1,3 @@
+export const calcSafeDimensions = () => {
+  return { width: (window.innerWidth * 0.8), height: (window.innerHeight * 0.875) };
+}
---
+++ src/p5/functions/index.ts
@@ -0,0 +1,1 @@
+export { calcSafeDimensions } from './_calcSafeDimensions';
