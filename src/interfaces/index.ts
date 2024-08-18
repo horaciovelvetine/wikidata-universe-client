7c144fc0e4bf806bdf165cd92de3965bead8b806
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-18 16:08:04 -0600

    add enum for input state validation and tracking

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -4,3 +4,5 @@ export type { iEdge } from './_Edge';
 export type { Point3D } from './_Point3D';
 export type { SessionData } from './_SessionData';
 export type { iVertex } from './_Vertex';
+
+export { eInputState } from './_InputState';
