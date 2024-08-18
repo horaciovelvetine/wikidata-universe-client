5c94e49cd16db0e4296efa0f6d302ed167d655e2
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-17 21:40:48 -0600

    is toggling state correctly without rerenders or weird side effects, still unable to pass state w/o render for hover maintain through state, but refining logic and appearance first
    no animations fully working just catches up to working out from sketch point now

--- src/app/WikiverseApp.tsx
+++ src/app/WikiverseApp.tsx
@@ -1,11 +1,12 @@
 import './WikiverseAppStyle.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ApiStatus, SessionData } from '../interfaces';
-import { VerticalSiteTitle, Footer } from '../components';
+import { VerticalSiteTitle, Footer, VerTextDetails, HorizonSiteTitle } from '../components';
 import { Vertex } from '../p5/models';
 import { WikiverseSketch } from '../p5/WikiverseSketch';
 
 import SessionDataMock from '../assets/data/session-body-r1-1.json';
+import { calcSafeDimensions } from '../p5/functions';
 
 interface WikiverseAppProps {
   apiStatusRes: ApiStatus;
@@ -19,15 +20,30 @@ const res1: SessionData = {
   queue: SessionDataMock.queue,
 };
 
+const WikiverseMemo = React.memo(WikiverseSketch, (prev, next) => {
+  return prev.session === next.session;
+});
+
 export const WikiverseApp: React.FC<WikiverseAppProps> = ({ apiStatusRes }) => {
+  const dimensions = calcSafeDimensions();
   const [query, setQuery] = useState<string>(res1.query!);
-  const [curSelVertex, setCurSelVertex] = useState<Vertex>();
+  const [vertexSelected, setVertexSelected] = useState<Vertex | null>(null);
+
+  useEffect(() => {
+    console.log('vertexSelected:', vertexSelected);
+  }, [vertexSelected]);
+
 
   return (
-    <>
+    <div id='wikiverse-container'>
       <VerticalSiteTitle />
-      <WikiverseSketch session={res1} />
+      <div id='sketch-container' style={{ width: dimensions.width, height: dimensions.height }}>
+        <WikiverseMemo session={res1} setCurSelection={setVertexSelected} />
+        <div id='sketch-overlay-bot'>
+          <VerTextDetails vertex={vertexSelected} />
+        </div>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 };
