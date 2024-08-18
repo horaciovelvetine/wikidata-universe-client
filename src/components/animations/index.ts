d885c4ae4a2bb0c98592f10007d58e5ec5497d03
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-18 16:35:27 -0600

    adds horizontal shake from v1

--- src/components/animations/index.ts
+++ src/components/animations/index.ts
@@ -1,5 +1,6 @@
 export { changeFocusOpacity } from './_ChangeFocusOpacity';
 export { flashNoneSelectedMsg } from './_FlashNoneSelectedMsg';
+export { horizontalShake } from './_HorizontalShake';
 export { flashOverlayElement } from './_FlashOverlayElement';
 export { rotateChevIcon } from './_RotateChevIcon';
 export { showHideAttrList } from './_ShowHideAttrList';
