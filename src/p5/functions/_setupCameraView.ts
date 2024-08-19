37863f3f15d14b15dbfc19f67bdd9ce0bea68cd1
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-18 20:49:00 -0600

    ensures return value is aware of type

--- src/p5/functions/_setupCameraView.ts
+++ src/p5/functions/_setupCameraView.ts
@@ -1,7 +1,7 @@
 import { P5CanvasInstance } from "@p5-wrapper/react";
 import { Camera } from "p5";
 
-export function setupCameraView(p5: P5CanvasInstance, cam: Camera) {
+export function setupCameraView(p5: P5CanvasInstance, cam: Camera): Camera {
   cam = p5.createCamera();
   cam.setPosition(0, 0, 100);
   cam.lookAt(0, 0, 0);
