90650d227ec4f3dffefb0c26f67ff4306dbab333
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-20 22:08:38 -0600

    creates a point 3D model for animating the cmaera tracking

---
+++ src/p5/models/_Point3D.ts
@@ -0,0 +1,12 @@
+import { iPoint3D } from "../../interfaces";
+
+export class Point3D implements iPoint3D {
+  x: number;
+  y: number;
+  z: number;
+  constructor(x: number, y: number, z: number) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+}
