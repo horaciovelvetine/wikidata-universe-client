9c96b4717551d729c7dbb1a3bd110c9ffe09fa41
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-20 18:51:22 -0600

    adds dynamic button for fetching state representation visually and animates
    adds current refs const to prevent loosing usable refs on rerender problem
    
    newQuery handler is successfully bubbling up init request for new data and passing invalid when returning an error

--- src/components/_ActiveQueryControls.tsx
+++ src/components/_ActiveQueryControls.tsx
@@ -1,18 +1,20 @@
 import './_ActiveQueryControlsStyle.css';
 import './animations/_HorizontalShake.css';
 import React, { createRef, useEffect, useState } from 'react';
-import { eInputState } from '../interfaces';
+import { eInputState, SessionData } from '../interfaces';
 import { Search, SearchDngr, Fetch } from '../assets/icons';
 import { flashOverlayElement, shakeInvalidElement } from './animations';
-import { inputStateFromValue, inputValueIsEmpty } from './util';
+import { inputStateFromValue, queryIsSuccess, inputIsUseable } from './util';
+import { getQueryData } from '../api';
+
 
 interface ActiveQueryControlsProps {
   curQuery: string | undefined;
   camFocusHandler: (target: string) => boolean;
-  submitQueryHandler: (query: string) => void;
+  newQueryHandler: (data: SessionData) => void;
 }
 
-export const ActiveQueryControls: React.FC<ActiveQueryControlsProps> = ({ curQuery, camFocusHandler, submitQueryHandler }) => {
+export const ActiveQueryControls: React.FC<ActiveQueryControlsProps> = ({ curQuery, camFocusHandler, newQueryHandler }) => {
   const [query, setQuery] = useState(curQuery);
   const [input, setInput] = useState<eInputState>(eInputState.DEFAULT);
   const [fetching, setFetching] = useState(false);
@@ -28,30 +30,57 @@ export const ActiveQueryControls: React.FC<ActiveQueryControlsProps> = ({ curQue
     setInput(inputStateFromValue(e.target.value));
   }
 
-  function handleQuerySubmit(e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>) {
-    e.preventDefault();
-    if (input == eInputState.VALID || input == eInputState.DEFAULT) {
-      const tgtInExistingSet = camFocusHandler(query!);
-      if (tgtInExistingSet) return;
-      setFetching(true);
-      submitQueryHandler(query!);
-      setFetching(false);
-      return;
+  const currentRefs = () => {
+    return {
+      cont: contRef.current!, input: inputRef.current!,
+      submit: submitRef.current!, icon: iconRef.current!,
+      iconDng: iconDngrRef.current!,
     }
-    shakeInvalidElement(contRef.current!);
-    shakeInvalidElement(inputRef.current!);
-    flashOverlayElement(iconDngrRef.current!, iconRef.current!, 820);
-    shakeInvalidElement(submitRef.current!);
   }
 
-  return curQuery == undefined ? <></> : (
+  async function handleQuerySubmit(e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+
+    const els = currentRefs();
+
+    if (inputIsUseable(input)) {
+      const tgtInExistingSet = camFocusHandler(query!);
+      if (tgtInExistingSet) return; //already in set, no need to fetch
+
+      setFetching(true);
+      const res = await getQueryData(query!);
+      setFetching(false);
+      if (queryIsSuccess(res)) {
+        console.log(res)
+        newQueryHandler(res.data as SessionData);
+        return;
+      }
+      // todo - display error message from data[?]
+    }
+    shakeInvalidElement(els.cont);
+    shakeInvalidElement(els.input);
+    flashOverlayElement(els.iconDng, els.icon, 820);
+    shakeInvalidElement(els.submit);
+  }
+
+
+  const dynamicButton = () => {
+    return (
+      <button id='aqc-query-submit' type='submit' onClick={handleQuerySubmit} ref={submitRef}>
+        <img id='aqc-invalid-query-icon' src={SearchDngr} alt='magnifying glass search' ref={iconDngrRef} />
+        {fetching ? <img id='aqc-query-fetching-icon' src={Fetch} alt='fetching data' /> :
+          <>
+            <img id='aqc-query-submit-icon' src={Search} alt='magnifying glass search' ref={iconRef} />
+          </>}
+      </button>
+    );
+  }
+
+  return (
     <div id='aqc-container' ref={contRef}>
       <form id='aqc-query-form' onSubmit={handleQuerySubmit}>
         <input id='aqc-query-input' type='text' value={query} onChange={handleInputChanges} ref={inputRef} />
-        <button id='aqc-query-submit' type='submit' onClick={handleQuerySubmit} ref={submitRef}>
-          <img id='aqc-query-submit-icon' src={fetching ? Fetch : Search} alt='magnifying glass search' ref={iconRef} />
-          <img id='aqc-invalid-query-icon' src={fetching ? Fetch : SearchDngr} alt='magnifying glass search' ref={iconDngrRef} />
-        </button>
+        {dynamicButton()}
       </form>
     </div>
   );
