a7a5fbeab4d882ccaaa1d50340722f7ee1b56a1b
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-20 22:08:16 -0600

    remove all errant error causing at top for know, will integrate later

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,8 @@
 // import './styles/Global.css'
 // import './styles/Fonts.css'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { WikiverseApp } from './app/WikiverseApp'
-import { getApiStatus } from './api'
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <WikiverseApp apiStatusRes={await getApiStatus()} />
-  </React.StrictMode>
+  <WikiverseApp apiStatusRes={{ code: 200, message: "Ok" }} />
 )
