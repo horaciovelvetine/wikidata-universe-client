d450124e471f3d5fadcb48854f6bc2f573cfe113
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-22 20:16:52 -0600

    move sketch func to this folder, and models can handle their own drawings and helpers

--- src/p5/functions/index.ts
+++ src/p5/functions/index.ts
@@ -1,5 +1,6 @@
 export { calcSafeDimensions } from './_calcSafeDimensions';
+export { calcVertexSetMean } from './_calcVertexSetMean';
+export { drawSketchUI } from './_drawSketchUI';
+export { minMaxValuesInSet } from './_minMaxValuesInSet';
 export { setupCameraView } from './_setupCameraView';
 export { traceRay } from './_traceRay';
-export { minMaxValuesInSet } from './_minMaxValuesInSet';
-export { calcVertexSetMean } from './_calcVertexSetMean';
