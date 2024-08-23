35668b16b091261902d1cb38d78593b84b69b26a
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-22 20:54:25 -0600

    cleans up and removes duplicate hover call/check, vertex back to just drawing itself and not being needed locally

--- src/p5/WikiverseSketch.tsx
+++ src/p5/WikiverseSketch.tsx
@@ -3,7 +3,7 @@ import { P5CanvasInstance, ReactP5Wrapper, Sketch } from "@p5-wrapper/react";
 import { calcSafeDimensions, setupCameraView, traceRay, drawSketchUI } from "./functions";
 import { SessionData } from "../interfaces";
 import { Camera, Font } from "p5";
-import { Vertex, Point3D } from "./models";
+import { Vertex, Point3D, LookAtChange } from "./models";
 import CharisBold from "../assets/font/CharisSIL-Bold.ttf";
 
 interface WikiverseSketchProps {
@@ -14,16 +14,11 @@ interface WikiverseSketchProps {
 
 export const WikiverseSketch: React.FC<WikiverseSketchProps> = ({ session, setCurSelection, setCam }) => {
   let curHoveredVert: Vertex | null;
-  let cam: Camera;
   let curSelectedVert: Vertex | null;
+  let lookAt: LookAtChange = new LookAtChange(null);
+  let cam: Camera;
   let font: Font;
-  let lookAtTarget: Point3D | null = null;
-  let lookAtKeyFrame = 0;
-  const animKeyframeDur = 120;
 
-  //?                      ?//
-  //? SKETCH SKETCH SKETCH ?//
-  //?                      ?//
   const WikiverseSketch: Sketch = (p5: P5CanvasInstance) => {
 
     //* ==>       <== *//
@@ -43,6 +38,7 @@ export const WikiverseSketch: React.FC<WikiverseSketchProps> = ({ session, setCu
     //* ==>      <== *//
     p5.draw = () => {
       drawSketchUI(p5, session);
+
       if (!!curSelectedVert) {
         curSelectedVert.drawLabel(p5, cam, font);
         curSelectedVert.drawRelatedEdges(p5, session);
@@ -54,26 +50,13 @@ export const WikiverseSketch: React.FC<WikiverseSketchProps> = ({ session, setCu
       }
 
       session.vertices.forEach((vDat) => {
-        const vert = new Vertex(vDat);
-        vert.draw(p5);
-
-        if (vert.id == curHoveredVert?.id) {
-          vert.drawLabel(p5, cam, font);
-        }
+        new Vertex(vDat).draw(p5);
       });
-      // Animate: lookAt() => { onClick() }
-      if (lookAtTarget && lookAtKeyFrame < animKeyframeDur) {
-        const kFMult = lookAtKeyFrame * 1.00001 / animKeyframeDur
-        const lookAtChngVec = p5.createVector(
-          p5.lerp(cam.centerX, lookAtTarget.x, kFMult),
-          p5.lerp(cam.centerY, lookAtTarget.y, kFMult),
-          p5.lerp(cam.centerZ, lookAtTarget.z, kFMult)
-        )
-        cam.lookAt(lookAtChngVec.x, lookAtChngVec.y, lookAtChngVec.z)
-        lookAtKeyFrame += 1;
-      } else {
-        lookAtTarget = null;
-        lookAtKeyFrame = 0;
+
+      if (lookAt.hasTarget() && lookAt.animInProgress()) {
+        const vec = lookAt.changeVec(p5, cam);
+        cam.lookAt(vec.x, vec.y, vec.z)
+        lookAt.advance();
       }
     };
 
@@ -83,13 +66,15 @@ export const WikiverseSketch: React.FC<WikiverseSketchProps> = ({ session, setCu
     p5.mouseClicked = () => {
       session.vertices.forEach((vertex) => {
         const vert = new Vertex(vertex);
+
         const vertexClicked = traceRay(p5, cam, vert);
         if (!vertexClicked) return;
+
         if (curSelectedVert != null && curSelectedVert.id == vert.id) {
           curSelectedVert = null
           setCurSelection(null);
         } else {
-          lookAtTarget = new Point3D(vert.coords.x, vert.coords.y, vert.coords.z)
+          lookAt.setTarget(vert.coords);
           curSelectedVert = vert;
           setCurSelection(vert);
         }
@@ -104,11 +89,13 @@ export const WikiverseSketch: React.FC<WikiverseSketchProps> = ({ session, setCu
         const isStillHoveredVert = traceRay(p5, cam, curHoveredVert)
         if (isStillHoveredVert) return;
       }
+
       let foundHoveredVert = false;
       session.vertices.forEach(vert => {
         const checkVert = new Vertex(vert);
         if (curSelectedVert?.id == checkVert.id) return;
         const newVertIsHovered = traceRay(p5, cam, checkVert);
+
         if (newVertIsHovered) {
           curHoveredVert = checkVert;
           foundHoveredVert = true;
