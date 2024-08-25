55f5908096eba403321f653682700502a859ef61
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-25 12:37:39 -0600

    init edge details display for initial edge from sample set with static details mainly sorting out initial ideas and prototyping

---
+++ src/components/_VertexEdgeDetails.tsx
@@ -0,0 +1,64 @@
+import './_VertexEdgeDetailsStyle.css'
+import React, { createRef, useEffect, useState } from 'react';
+import { ArrowBoth, ArrowFrom, ArrowTo } from '../assets/icons';
+import { Vertex } from '../p5/models';
+
+interface VertexEdgeDetailsProps {
+  vertex: Vertex | null;
+}
+
+export const VertexEdgeDetails: React.FC<VertexEdgeDetailsProps> = ({ vertex }) => {
+  const [isOnScreen, setIsOnScreen] = useState(false);
+
+  const contRef = createRef<HTMLDivElement>();
+  const edgesInfoRef = createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    if (vertex == null) {
+      setIsOnScreen(false);
+    } else {
+      setIsOnScreen(true);
+    }
+  }, [vertex])
+
+  useEffect(() => {
+    // animate refs...
+  }, [isOnScreen])
+
+
+  return (
+    <div id='edges-info-container' ref={contRef}>
+      {/* permanenet on screen */}
+      <div id='edges-info' ref={edgesInfoRef}>
+        {/* animate onto the screen when 'isOnScreen' */}
+        <div id='edge-details'>
+          <p>Charles</p>
+          <img id='parallel-icon' src={ArrowBoth} alt='parallel edge' />
+          <p>Edge.B</p>
+          <p>(property.label)</p>
+        </div>
+        <div id='edge-details'>
+          <p>Charles</p>
+          <img id='parallel-icon' src={ArrowTo} alt='parallel edge' />
+          <p>Edge.B</p>
+          <p>(property.label)</p>
+        </div>
+        <div id='edge-details'>
+          <p>Charles</p>
+          <img id='parallel-icon' src={ArrowTo} alt='parallel edge' />
+          <p>Edge.B</p>
+          <p>(property.label)</p>
+        </div>
+        <div id='edge-details'>
+          <p>Charles</p>
+          <img id='parallel-icon' src={ArrowFrom} alt='parallel edge' />
+          <p>Edge.B</p>
+          <p>(property.label)</p>
+        </div>
+        <div id='title-container'>
+          <p id='title'>edges_</p>
+        </div>
+      </div>
+    </div>
+  );
+};
