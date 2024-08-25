1f47770a8c4fc0b00784215a3c46f18f475c39ec
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-25 11:17:35 -0600

    adds selected history working to add members as selected and excludes needed
    sketch informs react when a vertex is selected and displaying correct vertex details

--- src/p5/WikiverseSketch.tsx
+++ src/p5/WikiverseSketch.tsx
@@ -3,14 +3,20 @@ import CharisTTF from "../assets/font/CharisSIL-Regular.ttf";
 import { P5CanvasInstance, ReactP5Wrapper, Sketch } from "@p5-wrapper/react";
 import { Camera, Font } from "p5";
 
-import { calcSafeDimensions, setupCameraView, traceRay, drawSketchUI } from "./functions";
-import { Vertex, LookAtChange } from "./models";
+import { calcInitLayoutDimensions, setupCameraView, traceRay, drawSketchUI } from "./functions";
+import { Vertex, LookAtChange, Edge } from "./models";
+import { clickTargetMaatchesCurrentSelection, vertexInSelectedHistory } from "./util";
 
 import CharlesSessionDataR2 from '../assets/data/charles-data-r1-2.json'
 import { SessionData } from "../interfaces";
-import { postRelatedDataClick } from "../api";
 
 
+interface WikiverSketchProps {
+  query: string | undefined;
+  setSelectedVertex: React.Dispatch<React.SetStateAction<Vertex | null>>;
+  setDisplayedEdgesDetails: React.Dispatch<React.SetStateAction<Edge[]>>;
+  setSelectedHistory: React.Dispatch<React.SetStateAction<Vertex[]>>;
+}
 
 const sessionData = (): SessionData => {
   return {
@@ -20,11 +26,11 @@ const sessionData = (): SessionData => {
     edges: CharlesSessionDataR2.edges,
     properties: CharlesSessionDataR2.properties,
     queue: CharlesSessionDataR2.queue,
-    dimensions: calcSafeDimensions()
+    dimensions: calcInitLayoutDimensions()
   }
 }
 
-export const WikiverseSketch: React.FC = () => {
+export const WikiverseSketch: React.FC<WikiverSketchProps> = ({ setSelectedVertex }) => {
   // SKETCH 
   let wikiFont: Font;
   let cam: Camera;
@@ -33,7 +39,7 @@ export const WikiverseSketch: React.FC = () => {
   let selectedVertex: Vertex | null;
   let hoveredVertex: Vertex | null;
   let session = sessionData();
-  let clickedTargets: string[] = [];
+  let selectedHistory: Vertex[] = [];
 
   const WikiverseSketch: Sketch = (p5: P5CanvasInstance) => {
 
@@ -42,7 +48,7 @@ export const WikiverseSketch: React.FC = () => {
     //* ==>       <== *//
     p5.preload = () => { wikiFont = p5.loadFont(CharisTTF); };
     p5.setup = () => {
-      const { width, height } = calcSafeDimensions();
+      const { width, height } = calcInitLayoutDimensions();
       p5.createCanvas(width, height, p5.WEBGL);
       p5.textFont(wikiFont!); //set in preload
       cam = setupCameraView(p5, cam);
@@ -70,9 +76,9 @@ export const WikiverseSketch: React.FC = () => {
         new Vertex(vDat).draw(p5);
       });
 
-      if (lookAt.hasTarget() && lookAt.animInProgress()) {
-        const vec = lookAt.changeVec(p5, cam!);
-        cam!.lookAt(vec.x, vec.y, vec.z)
+      if (lookAt.animInProgress()) {
+        const { x, y, z } = lookAt.changeVec(p5, cam!);
+        cam!.lookAt(x, y, z)
         lookAt.advance();
       }
     };
@@ -83,30 +89,23 @@ export const WikiverseSketch: React.FC = () => {
     p5.mouseClicked = () => {
       session.vertices.forEach((vertexData) => {
         const vert = new Vertex(vertexData);
-        const vertexClicked = traceRay(p5, cam!, vert);
-        if (!vertexClicked) return;
+        if (!traceRay(p5, cam!, vert)) return; // vertex is not click target
 
-        if (selectedVertex != null && selectedVertex.id == vert.id) {
+        if (clickTargetMaatchesCurrentSelection(selectedVertex, vert)) {
           // Deselect current vertex
           selectedVertex = null;
+          setSelectedVertex(null);
           return;
-        } else {
-          // Select a new vertex
-          const originalQuery = session.query;
-          lookAt.setTarget(vert.coords);
-          selectedVertex = vert;
-          // Skip fetching things we've already clicked on
-          if (clickedTargets.includes(vert.id)) return;
-
-          // Fetch new related vertex data...
-          postRelatedDataClick({ ...session, query: vert.label })
-            .then(res => {
-              clickedTargets.push(vert.id)
-              session = { ...res.data as SessionData, query: originalQuery };
-            }).catch(e => {
-              console.log(e)
-            });
         }
+        // Select new Vertex
+        lookAt.setTarget(vert.coords);
+        selectedVertex = vert;
+        setSelectedVertex(vert);
+        // Fetch new Vertex details...
+        if (vert.label == session.query) return; // origin already fetched...
+        if (vertexInSelectedHistory(selectedHistory, vert)) return; // already fetched...
+        selectedHistory.push(vert);
+        console.log('look for new details...')
       })
     };
 
@@ -115,8 +114,7 @@ export const WikiverseSketch: React.FC = () => {
     //* ==>       <== *//
     p5.mouseMoved = () => {
       if (hoveredVertex != null) {
-        const isStillHoveredVert = traceRay(p5, cam!, hoveredVertex)
-        if (isStillHoveredVert) return;
+        if (traceRay(p5, cam!, hoveredVertex)) return; // same vertex still hovered
       }
 
       let foundHoveredVert = false;
@@ -139,7 +137,7 @@ export const WikiverseSketch: React.FC = () => {
     //* ==> RESIZE <== *//
     //* ==>        <== *//
     p5.windowResized = () => {
-      const { width, height } = calcSafeDimensions();
+      const { width, height } = calcInitLayoutDimensions();
       p5.resizeCanvas(width, height);
     };
 
