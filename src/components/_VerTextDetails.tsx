15bdac12b05fc441c4a65b25745616276da8f658
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-25 12:35:49 -0600

    fixes grid position and icon refs to new exported index values

--- src/components/_VerTextDetails.tsx
+++ src/components/_VerTextDetails.tsx
@@ -1,11 +1,11 @@
 import './_VerTextDetailsStyle.css';
-
-import { Vertex } from '../p5/models';
-import { changeFocusOpacity, flashNoneSelectedMsg, flashOverlayElement, rotateChevIcon, showHideAttrList } from './animations';
 import React, { createRef, useEffect, useState } from 'react';
-import ChevIcon from '../assets/icons/mi-chev-icon.svg';
-import ChevDangerIcon from '../assets/icons/mi-chev-icon-danger.svg';
-import LinkIcon from '../assets/icons/mi-link-icon.svg';
+
+import { changeFocusOpacity, flashNoneSelectedMsg, flashOverlayElement, rotateChevIcon, showHideAttrList } from './animations';
+import { Chev, ChevDngr, Link } from '../assets/icons'
+import { Vertex } from '../p5/models';
+
+
 
 interface VerTextDetailsProps {
   vertex: Vertex | null;
@@ -51,8 +51,8 @@ export const VerTextDetails: React.FC<VerTextDetailsProps> = ({ vertex }) => {
           <ul id='attr-list' ref={attrListRef}>
             <li id='attr'>{vertex != null ? <><span id='attr-lbl'>label_ </span>{vertex.label}</> : ''}</li>
             <li id='attr'>{vertex != null ? <><span id='attr-lbl'>desc_ </span> {vertex.description}</> : ''}</li>
-            <li id='attr'>{vertex != null ? <a id='attr-link' href={vertex != null ? vertex.url() : ''} target='_blank' rel='noreferrer'>
-              <img src={LinkIcon} id='attr-link-icon' alt='Wikipedia link icon' />
+            <li id='attr'>{vertex != null ? <a id='attr-link' href={vertex != null ? vertex.url() : ''} target='_blank' rel='noreferrer' >
+              <img src={Link} id='attr-link-icon' alt='Wikipedia link icon' />
             </a> : ''}
             </li>
           </ul>
@@ -60,8 +60,8 @@ export const VerTextDetails: React.FC<VerTextDetailsProps> = ({ vertex }) => {
         </div>
         <button id='icon-btn' onClick={handleVerTextToggleClick} >
           <div id='icon-container'>
-            <img src={ChevDangerIcon} id='icon-danger' ref={chevDangRef} alt='Invalid action indicator' />
-            <img src={ChevIcon} id='icon' ref={chevIconRef} alt='Chevron toggle indicator' />
+            <img src={ChevDngr} id='icon-danger' ref={chevDangRef} alt='Invalid action indicator' />
+            <img src={Chev} id='icon' ref={chevIconRef} alt='Chevron toggle indicator' />
           </div>
         </button>
       </div>
