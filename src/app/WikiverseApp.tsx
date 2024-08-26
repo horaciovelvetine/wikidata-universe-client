ef968a972ae9ba868dbc60a6425cc7b3ab2afb8a
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-25 20:52:33 -0600

    init back related state triggers to pass info back into React App from sketch

--- src/app/WikiverseApp.tsx
+++ src/app/WikiverseApp.tsx
@@ -1,33 +1,44 @@
 import './WikiverseAppStyle.css';
-import React, { useEffect, useState } from 'react';
-import { ApiStatus } from '../interfaces';
-import { VerticalSiteTitle, Footer, VerTextDetails, ActiveQueryControls } from '../components';
+import React, { useEffect, useState, memo } from 'react';
+import { ApiStatus, SketchData, } from '../interfaces';
+import { VerticalSiteTitle, Footer, VerTextDetails, ActiveQueryControls, RelatedEdgesDetails } from '../components';
 
 import { WikiverseSketch } from '../p5/WikiverseSketch';
+import { calcInitLayoutDimensions } from '../p5/functions';
+import { Vertex } from '../p5/models';
+import { Camera } from 'p5';
 
-import { calcSafeDimensions } from '../p5/functions';
 
 
 interface WikiverseAppProps {
   apiStatusRes: ApiStatus;
 }
 
+const MemoizedSketch = memo(WikiverseSketch, (prevProps, nextProps) => {
+  return prevProps.query == nextProps.query
+})
+
 export const WikiverseApp: React.FC<WikiverseAppProps> = () => {
-  const [dimensions, setDimensions] = useState(calcSafeDimensions());
+  const [dimensions, setDimensions] = useState(calcInitLayoutDimensions());
+  const [query, setQuery] = useState<string>("Charles");
+  const [selectedVertex, setSelectedVertex] = useState<Vertex | null>(null);
+  const [sketchData, setSketchData] = useState<SketchData>();
+  const [camRef, setCamRef] = useState<Camera>();
 
   useEffect(() => {
-    window.addEventListener('resize', () => setDimensions(calcSafeDimensions()));
-    return () => window.removeEventListener('resize', () => setDimensions(calcSafeDimensions()));
+    window.addEventListener('resize', () => setDimensions(calcInitLayoutDimensions()));
+    return () => window.removeEventListener('resize', () => setDimensions(calcInitLayoutDimensions()));
   }, [])
 
   return (
     <div id='wikiverse-container'>
       <VerticalSiteTitle />
       <div id='sketch-container' style={{ width: dimensions.width, height: dimensions.height }}>
-        <WikiverseSketch />
+        <MemoizedSketch {...{ query, setSelectedVertex, setSketchData, setCamRef }} />
         <div id='sketch-overlay-bot'>
-          <ActiveQueryControls curQuery={'test'} />
-          <VerTextDetails vertex={null} />
+          <RelatedEdgesDetails vertex={selectedVertex} data={sketchData} />
+          <ActiveQueryControls curQuery={query} />
+          <VerTextDetails vertex={selectedVertex} />
         </div>
       </div>
       <Footer />
