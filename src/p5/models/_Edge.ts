d4a7309cc7b6253930c757640109bf4c66c85fd1
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-25 20:58:02 -0600

    adds helpers for use to get relevant data as needed, deliberately avoiding graphset to let models handle this logic
    already pretty sprawling

--- src/p5/models/_Edge.ts
+++ src/p5/models/_Edge.ts
@@ -1,4 +1,6 @@
-import { iEdge } from "../../interfaces";
+import { iEdge, SketchData } from "../../interfaces";
+import { Property } from "./_Property";
+import { Vertex } from "./_Vertex";
 
 export class Edge implements iEdge {
   srcId: string;
@@ -6,12 +8,25 @@ export class Edge implements iEdge {
   propertyId: string;
   label: string | null;
   type: string;
-  constructor(edge: iEdge);
-  constructor(edge: Edge) {
-    this.srcId = edge.srcId;
-    this.tgtId = edge.tgtId;
-    this.propertyId = edge.propertyId;
-    this.label = edge.label;
-    this.type = edge.type;
+  constructor(edgeData: iEdge) {
+    this.srcId = edgeData.srcId;
+    this.tgtId = edgeData.tgtId;
+    this.propertyId = edgeData.propertyId;
+    this.label = edgeData.label;
+    this.type = edgeData.type;
+  }
+
+  getVertexEndpoints(data: SketchData) {
+    const endPoints = data.vertices.filter(vertexData =>
+      (vertexData.id == this.srcId || vertexData.id == this.tgtId)
+    )
+    if (this.srcId == endPoints[0].id) {
+      return { src: new Vertex(endPoints[0]), tgt: new Vertex(endPoints[1]) }
+    }
+    return { src: new Vertex(endPoints[1]), tgt: new Vertex(endPoints[0]) }
+  }
+
+  getPropertyDetails(data: SketchData): Property {
+    return data.properties.find(property => this.propertyId == property.id)
   }
 }
