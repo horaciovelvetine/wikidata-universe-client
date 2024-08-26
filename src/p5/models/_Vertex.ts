cde1ebdb6ce13595a637957d33de0169d0d1bc12
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-25 22:51:41 -0600

    adds some comments for docs to seperate out and explain to ref in the near future

--- src/p5/models/_Vertex.ts
+++ src/p5/models/_Vertex.ts
@@ -48,21 +48,31 @@ export class Vertex implements iVertex {
 
     edgesToDraw.forEach(edge => {
       const vert2 = this.getAltVertex(session, edge)!;
-      const isPara = this.isParallelEdge(vert2, edge!, edgesToDraw)
+      const isPara = this.parallelEdges(vert2, edge!, edgesToDraw)
       const { x, y, z } = this.coords;
       const { x: x2, y: y2, z: z2 } = vert2.coords;
 
       p5.push();
-      this.setEdgeStrokeColor(p5, edge!, isHov, isPara);
+      this.setEdgeStrokeColor(p5, edge!, isHov, (isPara.length > 0));
       p5.line(x, y, z, x2, y2, z2);
       p5.pop();
     });
   }
 
+  /**
+   * The English Wikipedia Article for the given Wikidata Vertex (if it exists).
+   * @see URL is a guess, Wikidata Entities do not always have article representations.
+   */
   url() {
     return `https://en.wikipedia.org/wiki/${this.label.replace(" ", "_")}`;
   }
 
+  /**
+   * Gets a list of edges where this vertex's id is used (as either src || tgt), and excludes any edges which are missing some piece of the data.
+   * 
+   * @param {SketchData} session - Data structure containing the vertex, and to check against
+   * @return {Edge[]} - Returns an array of data complete edges.
+   */
   getRelatedEdges(session: SketchData): Edge[] {
     const mentionEdge = session.edges.filter(edge => (this.id == edge.srcId || this.id == edge.tgtId));
     const completeEdges = mentionEdge.filter(edge => this.getAltVertex(session, edge) != undefined);
@@ -72,44 +82,31 @@ export class Vertex implements iVertex {
     })
   }
 
-  private getAltVertex(session: SketchData, edge: iEdge): Vertex | null {
-    const altVertexId = edge.srcId === this.id ? edge.tgtId : edge.srcId;
-    const altVertexData = session.vertices.find(v => v.id === altVertexId);
-
-    if (altVertexData) {
-      return new Vertex(altVertexData);
-    }
-    return null;
-  }
-
-  private setEdgeStrokeColor(p5: P5CanvasInstance, edge: iEdge, isHov: boolean, isParallel: boolean) {
-    const opacity = isHov ? 0.4 : 1;
-    const incomingColor = `rgba(30,0,255,${opacity})`;
-    const outgoingColor = `rgba(255,50,80,${opacity})`;
-    const bothColor = `rgba(135,20,255,${opacity})`;
-
-    if (isParallel) {
-      p5.stroke(bothColor);
-    } else if (edge.srcId === this.id) {
-      p5.stroke(outgoingColor);
-    } else if (edge.tgtId === this.id) {
-      p5.stroke(incomingColor);
-    }
-  }
-
-  private isParallelEdge(vert2: iVertex, edge: iEdge, edgesToDraw: iEdge[]): boolean {
+  /**
+ * Determines if there is a parallel edge for a given vertex and edge in a list of edges.
+ * A parallel edge exists if there is another edge with the same source and target ids but flipped.
+ * 
+ * @param {iVertex} vert2 - The vertex to check against.
+ * @param {iEdge} edge - The edge to compare.
+ * @param {iEdge[]} edgesToDraw - The list of all edges.
+ * @return {Edge[]} - Returns an array of paralell edges, if there are any.
+ */
+  parallelEdges(vert2: iVertex, edge: iEdge, edgesToDraw: iEdge[]): Edge[] {
     const isTarget = edge.tgtId === vert2.id; //determine which is which src||tgt
 
     const parallelEdges = edgesToDraw.filter(e =>
       isTarget ? e.srcId === vert2.id : e.tgtId === vert2.id
     );
 
-    return parallelEdges.length !== 0; //true if found
+    return parallelEdges.map(edgeDat => { return new Edge(edgeDat) }); //true if found
   }
 
-  // The way that text & p5.js intertact when using WEBGL leaves a bit to be desired
-  // (another) Thank you to @camelCaseSensitive on {github} & @morejpeg on {youtube}
-  // Excellent tutorial for this approach ==> (https://www.youtube.com/watch?v=kJMx0F7e9QU)
+  /**
+   *  
+   * The way that text & p5.js intertact when using WEBGL leaves a bit to be desired
+   * (another) Thank you to @camelCaseSensitive on {github} & @morejpeg on {youtube}
+   * Excellent tutorial for this approach ==> (https://www.youtube.com/watch?v=kJMx0F7e9QU)
+   */
   private applyLabelPositionTransforms(p5: P5CanvasInstance<SketchProps>, cam: Camera) {
     const { pan, tilt } = this.calcCamerasAngles(p5, cam);
 
@@ -118,7 +115,9 @@ export class Vertex implements iVertex {
     p5.rotateY(-p5.PI / 2); // reasons unclear
     p5.rotateZ(p5.PI); // flip around to face (scenes) up
   }
-
+  /**
+   * Applies text formatting calls before drawing text to screen.
+   */
   private applyLabelTextSetup(p5: P5CanvasInstance<SketchProps>, font: Font) {
     p5.textSize(8);
     p5.fill('rgb(245, 245, 245)');
@@ -127,6 +126,10 @@ export class Vertex implements iVertex {
     p5.translate(0, (this.radius * -1.25), 0); // position over vertex
   }
 
+  /**
+   * Determine the pan and tilt of the current @Camera in relation to where it is currently focused.
+   * @returns - an object containing the pan & tilt {number} values.
+   */
   private calcCamerasAngles(p5: P5CanvasInstance<SketchProps>, cam: Camera) {
     const { ex, ey, ez } = { ...this.eye(cam) }
     const { fx, fy, fz } = { ...this.focal(cam) }
@@ -141,6 +144,37 @@ export class Vertex implements iVertex {
   }
   private focal(cam: Camera) {
     return { fx: cam.centerX, fy: cam.centerY, fz: cam.centerZ }
+  }
+
+  /**
+   * @return - The @Vertex object on the other end of the given @Edge 
+   */
+  private getAltVertex(session: SketchData, edge: iEdge): Vertex | null {
+    const altVertexId = edge.srcId === this.id ? edge.tgtId : edge.srcId;
+    const altVertexData = session.vertices.find(v => v.id === altVertexId);
+
+    if (altVertexData) {
+      return new Vertex(altVertexData);
+    }
+    return null;
+  }
+
+  /**
+   * Applies the color transformation used to draw an Edge based on its direction.
+   */
+  private setEdgeStrokeColor(p5: P5CanvasInstance, edge: iEdge, isHov: boolean, isParallel: boolean) {
+    const opacity = isHov ? 0.4 : 1;
+    const incomingColor = `rgba(30,0,255,${opacity})`;
+    const outgoingColor = `rgba(255,50,80,${opacity})`;
+    const bothColor = `rgba(135,20,255,${opacity})`;
+
+    if (isParallel) {
+      p5.stroke(bothColor);
+    } else if (edge.srcId === this.id) {
+      p5.stroke(outgoingColor);
+    } else if (edge.tgtId === this.id) {
+      p5.stroke(incomingColor);
+    }
   }
 
 }
