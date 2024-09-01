a9b93ad7cebdf222684d78bfe24fe0b943d6a874
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-31 18:15:57 -0600

    uses new request interface structs

--- src/api/_PostRelatedDataQueue.ts
+++ src/api/_PostRelatedDataQueue.ts
@@ -1,19 +1,17 @@
 import axios from "axios";
 import { API_URL } from "./_ApiUrl";
-import { RequestResponse, SessionData } from "../interfaces";
+import { RequestPayload, RequestResponse } from "../interfaces";
 
-export async function postRelatedDataQueue(payload: SessionData): Promise<RequestResponse> {
-
+export async function postRelatedDataQueue(payload: RequestPayload): Promise<RequestResponse> {
   return await axios.post(API_URL('related-data-queue'), payload)
     .then(res => {
-      return { status: res.status, data: res.data };
+      return { status: res.status, data: res.data, errMsg: undefined };
     })
     .catch(err => {
       console.log(err);
       if (err.code === 'ERR_NETWORK') {
-        return { status: 500, data: 'The WikiData Universe API is currently offline. Try again later.' };
+        return { status: 500, errMsg: 'The WikiData Universe API is currently offline. Try again later.', data: {} } as RequestResponse;
       }
-      debugger;
-      return { status: 404, data: 'Unknown Error Encountered.' };
+      return { status: 404, errMsg: 'Unknown Error Encountered.', data: {} } as RequestResponse;
     });
 }
