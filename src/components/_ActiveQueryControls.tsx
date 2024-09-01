72342352ae674417e9a7ff871a958a7e989e059c
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-08-31 18:17:42 -0600

    skipping default input and input handling all fixed from buggy external func behviors
    is sending requests and modifying correct state for intended use

--- src/components/_ActiveQueryControls.tsx
+++ src/components/_ActiveQueryControls.tsx
@@ -1,19 +1,20 @@
 import './_ActiveQueryControlsStyle.css';
 import './animations/_HorizontalShake.css';
 import React, { createRef, useEffect, useState } from 'react';
-import { INPUT_STATE, SessionData } from '../interfaces';
+import { INPUT_STATE, SketchData } from '../interfaces';
 import { Search, SearchDngr, Fetch } from '../assets/icons';
 import { flashOverlayElement, shakeInvalidElement } from './animations';
-import { inputStateFromValue, queryIsSuccess, inputIsUseable } from './util';
 import { getQueryData } from '../api';
 
 
 interface ActiveQueryControlsProps {
-  query: string | undefined;
+  curQuery: string;
+  setCurQuery: React.Dispatch<React.SetStateAction<string>>
+  setSketchData: React.Dispatch<React.SetStateAction<SketchData>>
 }
 
-export const ActiveQueryControls: React.FC<ActiveQueryControlsProps> = ({ query }) => {
-  const [input, setInput] = useState<string | undefined>(query);
+export const ActiveQueryControls: React.FC<ActiveQueryControlsProps> = ({ curQuery, setCurQuery, setSketchData }) => {
+  const [input, setInput] = useState<string>(curQuery);
   const [inputState, setInputState] = useState<INPUT_STATE>(INPUT_STATE.DEFAULT);
   const [fetching, setFetching] = useState(false);
 
@@ -25,7 +26,15 @@ export const ActiveQueryControls: React.FC<ActiveQueryControlsProps> = ({ query 
 
   function handleInputChanges(e: React.ChangeEvent<HTMLInputElement>) {
     setInput(e.target.value);
-    setInputState(inputStateFromValue(e.target.value));
+    if (curQuery == e.target.value) {
+      setInputState(INPUT_STATE.DEFAULT);
+    } else if (e.target.value.length == 0) {
+      setInputState(INPUT_STATE.EMPTY);
+    } else if (e.target.value.length > 0) {
+      setInputState(INPUT_STATE.VALID)
+    } else {
+      setInputState(INPUT_STATE.INVALID)
+    }
   }
 
   const currentRefs = () => {
@@ -38,23 +47,28 @@ export const ActiveQueryControls: React.FC<ActiveQueryControlsProps> = ({ query 
 
   async function handleQuerySubmit(e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
-
     const els = currentRefs();
 
-    if (inputIsUseable(inputState)) {
-      console.log('AQC=>SubmitHandler() todo')
-      // todo - display error message from data[?]
+    if (inputState === INPUT_STATE.VALID) {
+      setFetching(true);
+      const res = await getQueryData(input!)
+      setFetching(false);
+      if (res.status == 200) {
+        setSketchData(res.data as SketchData)
+        setCurQuery(input)
+      }
+    } else {
+      shakeInvalidElement(els.cont);
+      shakeInvalidElement(els.input);
+      flashOverlayElement(els.iconDng, els.icon, 820);
+      shakeInvalidElement(els.submit);
     }
-    shakeInvalidElement(els.cont);
-    shakeInvalidElement(els.input);
-    flashOverlayElement(els.iconDng, els.icon, 820);
-    shakeInvalidElement(els.submit);
   }
 
 
   const dynamicButton = () => {
     return (
-      <button id='aqc-query-submit' type='submit' onClick={handleQuerySubmit} ref={submitRef}>
+      <button id='aqc-query-submit' type='submit' onClick={(e) => handleQuerySubmit(e)} ref={submitRef}>
         <img id='aqc-invalid-query-icon' src={SearchDngr} alt='magnifying glass search' ref={iconDngrRef} />
         {fetching ? <img id='aqc-query-fetching-icon' src={Fetch} alt='fetching data' /> :
           <>
@@ -67,7 +81,7 @@ export const ActiveQueryControls: React.FC<ActiveQueryControlsProps> = ({ query 
   return (
     <div id='aqc-container' ref={contRef}>
       <form id='aqc-query-form' onSubmit={handleQuerySubmit}>
-        <input id='aqc-query-input' type='text' value={input} onChange={handleInputChanges} ref={inputRef} />
+        <input id='aqc-query-input' type='text' value={input} onChange={(e) => handleInputChanges(e)} ref={inputRef} />
         {dynamicButton()}
       </form>
     </div>
