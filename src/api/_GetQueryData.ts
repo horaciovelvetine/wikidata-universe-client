ab7df050e6a156056f7e2086f97e0d79e6b4e53b
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-09-06 15:50:17 -0600

    format and interface changes reflected in requests

--- src/api/_GetQueryData.ts
+++ src/api/_GetQueryData.ts
@@ -2,15 +2,18 @@ import axios from "axios";
 import { API_URL } from "./_ApiUrl";
 import { RequestResponse } from "../interfaces";
 
+import DemoRelatedQueueResponse from '../assets/data/r-hammond-demo-query-r1-2.json'
+
 export async function getQueryData(query: string): Promise<RequestResponse> {
   return await axios.get(API_URL('query-data'), { params: { query } })
     .then(res => {
-      return { status: res.status, data: res.data };
+      return { status: res.status, data: res.data, errMsg: undefined };
     })
     .catch(err => {
+      console.log('err', err)
       if (err.code === 'ERR_NETWORK') {
-        return { status: 500, data: 'The WikiData Universe API is currently offline. Try again later.' };
+        return { status: 200, data: DemoRelatedQueueResponse, errMsg: 'The WikiData Universe API is currently offline. Try again later.' };
       }
-      return { status: 404, data: 'Unknown Error Encountered.' };
+      return { status: 404, data: { query }, errMsg: 'Unknown Error Encountered.' } as RequestResponse;
     });
 }
--- src/api/_PostRelatedDataClick.ts
+++ src/api/_PostRelatedDataClick.ts
@@ -1,18 +1,18 @@
 import axios from "axios";
 import { API_URL } from "./_ApiUrl";
-import { RequestResponse, SessionData } from "../interfaces";
+import { RequestPayload, RequestResponse } from "../interfaces";
 
-export async function postRelatedDataClick(payload: SessionData): Promise<RequestResponse> {
+export async function postRelatedDataClick(payload: RequestPayload): Promise<RequestResponse> {
 
   return await axios.post(API_URL('related-data-click'), payload)
     .then(res => {
-      return { status: res.status, data: res.data };
+      return { status: res.status, data: res.data, errMsg: undefined };
     })
     .catch(err => {
       console.log(err);
       if (err.code === 'ERR_NETWORK') {
-        return { status: 500, data: 'The WikiData Universe API is currently offline. Try again later.' };
+        return { status: 500, data: {}, errMsg: 'The WikiData Universe API is currently offline. Try again later.' } as RequestResponse;
       }
-      return { status: 404, data: 'Unknown Error Encountered.' };
+      return { status: 404, data: {}, errMsg: 'Unknown Error Encountered.' } as RequestResponse;
     });
 }
