313edf797cf597d4f1c201130be69fcbefb4b4a7
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-09-06 15:52:47 -0600

    Vertex can be null

--- src/interfaces/_Vertex.ts
+++ src/interfaces/_Vertex.ts
@@ -1,8 +1,8 @@
-import { Point3D } from "./_Point3D";
+import { iPoint3D } from "./_Point3D";
 
 export interface iVertex {
   id: string;
   label: string;
-  description: string;
-  coords: Point3D;
+  description: string | null;
+  coords: iPoint3D;
 }
