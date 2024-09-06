10c516ace6254db28f5261f73f1b7b9ea3a43b6e
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-09-06 15:52:04 -0600

    changes to camera manager and init for new 'manager mode' to init some objects for sketch functions

---
+++ src/p5/models/_CameraManager.ts
@@ -0,0 +1,59 @@
+import { P5CanvasInstance } from "@p5-wrapper/react";
+import { Camera } from "p5";
+import { Point3D } from "./_Point3D";
+
+export class CameraManager {
+  p5: P5CanvasInstance;
+  cam: Camera | null;
+  target: Point3D | null;
+  currentKeyframe: number = 0;
+  duration: number = 120;
+
+  constructor(p5: P5CanvasInstance) {
+    this.p5 = p5;
+    this.target = new Point3D(null); // defaults to (0,0,0) / expected origin
+    this.cam = null;
+  }
+
+  advance() {
+    if (this.cam == null) return;
+    const { x, y, z } = this.changeVec();
+    this.cam.lookAt(x, y, z);
+
+    if (this.currentKeyframe == this.duration) {
+      this.target = null;
+      this.currentKeyframe = 0;
+      return;
+    }
+    this.currentKeyframe += 1;
+  }
+
+  animInProgress() {
+    return this.hasTarget() && this.currentKeyframe <= this.duration;
+  }
+
+  setTarget(point: Point3D) {
+    this.target = point;
+    this.currentKeyframe = 0;
+  }
+
+  setCamera(cam: Camera) {
+    this.cam = cam;
+  }
+
+  private hasTarget() {
+    return !!this.target;
+  }
+
+  private changeVec() {
+    // checks null.cam? above in @advance()
+    const mult = this.currentKeyframe * 1.001 / this.duration // mult prevents rnd bad  calcs for whole ints
+    return this.p5.createVector(
+      this.p5.lerp(this.cam!.centerX, this.target!.x, mult),
+      this.p5.lerp(this.cam!.centerY, this.target!.y, mult),
+      this.p5.lerp(this.cam!.centerZ, this.target!.z, mult),
+    )
+  }
+
+
+}
