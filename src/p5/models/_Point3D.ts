281e666b930854463a22d89de4529009c1be337c
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-09-06 15:53:31 -0600

    inits a 0 defaulter for points to not have supplied coords

--- src/p5/models/_Point3D.ts
+++ src/p5/models/_Point3D.ts
@@ -4,9 +4,15 @@ export class Point3D implements iPoint3D {
   x: number;
   y: number;
   z: number;
-  constructor(x: number, y: number, z: number) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
+  constructor(initPos: iPoint3D | null) {
+    if (initPos == null) {
+      this.x = 0;
+      this.y = 0;
+      this.z = 0;
+    } else {
+      this.x = initPos.x;
+      this.y = initPos.y;
+      this.z = initPos.z;
+    }
   }
 }
