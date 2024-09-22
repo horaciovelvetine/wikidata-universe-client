9fbdad53de7b015f47f465fe7d273ef2b0dfa690
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-09-22 16:44:38 -0600

    api and import/export shifts

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import { getQueryData } from './api'
+import { getApiStatus } from './api'
 import { MainAppLayout } from './app/MainAppLayout'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <MainAppLayout demoInitQueryRes={await getQueryData("Richard Hammond")} />
+  <MainAppLayout apiStatusResponse={await getApiStatus()} />
 )
