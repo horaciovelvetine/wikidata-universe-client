89e5d613bc96bb602ef2603f42d57a149b0e2d64
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-09-22 16:43:50 -0600

    reflects aligning status request to expect a response object which is fiercly consistent

--- src/api/_GetApiStatus.ts
+++ src/api/_GetApiStatus.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
-import { ApiStatus } from "../interfaces";
+import { RequestResponse } from "../interfaces";
 import { API_URL } from "./_ApiUrl";
 
-export async function getApiStatus(): Promise<ApiStatus> {
+export async function getApiStatus(): Promise<RequestResponse> {
   return await axios
     .get(API_URL("status"))
     .then((res) => {
-      return { code: res.status, message: res.data };
+      return { status: res.status, data: res.data, errMsg: undefined };
     }).catch(err => {
       if (err.code === "ERR_NETWORK") {
-        return { message: "The WikiData Universe API is currently offline. Try again later.", code: 500 };
+        return { errMsg: "The WikiData Universe API is currently offline. Try again later.", status: 500, data: {} } as RequestResponse;
       }
-      return { message: "Unknown Error Encountered.", code: 404 };
+      return { errMsg: "Unknown Error Encountered.", status: 404, data: {} } as RequestResponse;
     });
 }
