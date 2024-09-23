3237234237c93346f39908d75a0c79dd5c55bb3b
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-09-22 18:29:40 -0600

    fits data into expected interface for responses

--- src/api/_GetQueryData.ts
+++ src/api/_GetQueryData.ts
@@ -10,10 +10,10 @@ export async function getQueryData(query: string): Promise<RequestResponse> {
       return { status: res.status, data: res.data, errMsg: undefined };
     })
     .catch(err => {
-      console.log('err', err)
       if (err.code === 'ERR_NETWORK') {
-        return { status: 200, data: DemoRelatedQueueResponse, errMsg: 'The WikiData Universe API is currently offline. Try again later.' };
+        return { status: 400, data: {}, errMsg: 'The WikiData Universe API is currently offline. Try again later.' } as RequestResponse;
       }
+      debugger
       return { status: 404, data: { query }, errMsg: 'Unknown Error Encountered.' } as RequestResponse;
     });
 }
