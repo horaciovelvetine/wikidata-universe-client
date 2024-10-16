e3d690480974560e84d2001d3b20b69e9d0335c7
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-10-15 21:30:59 -0600

    updates to current and working as expected

--- src/components/_MainAppLayout/ApiOfflineNotice.tsx
+++ src/components/_MainAppLayout/ApiOfflineNotice.tsx
@@ -1,10 +1,8 @@
-import './ApiOfflineNoticeStyle.css'
-
-
-import { createRef, useEffect } from "react";
-
+import './ApiOfflineNotice.css'
 import { Exclaims, Question } from "../../assets/icons";
 import GlobeLogo from '../../assets/imgs/globe-outline-no-bg-white.svg'
+
+import { createRef, useEffect } from "react";
 
 import { RequestResponse } from '../../interfaces';
 import { toggleElementOpacity } from '../';
@@ -13,26 +11,32 @@ interface ApiOfflineProps {
   apiStatusResponse: RequestResponse;
 }
 
+const prfx = (sufx: string) => {
+  return 'api-offline-notice-' + sufx;
+}
+
 export const ApiOfflineNotice: React.FC<ApiOfflineProps> = ({ apiStatusResponse }) => {
 
   const containerRef = createRef<HTMLDivElement>();
 
   useEffect(() => {
-    toggleElementOpacity(containerRef.current!, true, '250ms');
+    setTimeout(() => {
+      toggleElementOpacity(containerRef.current!, true, '400ms');
+    }, 1)
   }, []);
 
   return (
     <>
-      <div id='error-message-container' ref={containerRef}>
-        <div id='error-text-container'>
-          <h1 id='error-title'>{apiStatusResponse.status} Unavailable</h1>
-          <p id='error-message'>{apiStatusResponse.errMsg}</p>
+      <div id={prfx('cont')} ref={containerRef}>
+        <div id={prfx('text-cont')}>
+          <h1 id={prfx('title')}>{apiStatusResponse.status} Unavailable</h1>
+          <p id={prfx('notice-msg')}>{apiStatusResponse.errMsg}</p>
         </div>
-        <div id='unavailable-glogo-container'>
-          <img src={GlobeLogo} alt="Wikipedia globe logo" id="unavailable-glogo" />
+        <div id={prfx('globe-cont')}>
+          <img src={GlobeLogo} alt="Wikipedia globe logo" id={prfx('globe-logo')} />
         </div>
-        <img src={Question} alt="Big ol' question mark" id="question-mark-icon" />
-        <img src={Exclaims} alt="Big ol' exclamation point (this error must be a problem)" id='exclamation-mark-icon' />
+        <img src={Question} alt="Big ol' question mark" id={prfx('question-icon')} />
+        <img src={Exclaims} alt="Big ol' exclamation point (this error must be a problem)" id={prfx('exclaims-icon')} />
       </div>
     </>
   );
