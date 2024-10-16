b7d45df3d756857c9bd3581faea302ae0faa9a20
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-10-16 00:39:21 -0600

    remove old no longer being used

--- src/p5/util/_clickTargetMatchesCurrentSelection.ts
+++
@@ -1,5 +0,0 @@
-import { Vertex } from "../models";
-
-export function clickTargetMatchesCurrentSelection(curSelected: Vertex | null, target: Vertex) {
-  return curSelected != null && curSelected.id == target.id;
-}
