f093ff55946fd816b43f19e416a3e830e409727f
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-10-15 22:43:03 -0600

    switches to default no unit provided for s or ms

--- src/components/animations/ToggleElementOpacity.ts
+++ src/components/animations/ToggleElementOpacity.ts
@@ -5,8 +5,8 @@
  * @param {boolean} show - A boolean indicating whether to show or hide the element.
  * @param {string} [duration='0.75'] - The duration of the fade effect in seconds or fraction of seconds.
  */
-export function toggleElementOpacity(ele: HTMLElement, show: boolean, duration: string = '0.75') {
-  ele.style.transition = `opacity ${duration}s ease-in-out`
+export function toggleElementOpacity(ele: HTMLElement, show: boolean, duration: string = '750ms') {
+  ele.style.transition = `opacity ${duration} ease-in-out`
   if (show) {
     ele.style.opacity = '1';
   } else {
