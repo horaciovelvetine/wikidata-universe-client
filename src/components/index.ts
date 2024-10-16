d2f49fab9ea6d61f754fb532f76548c8ed2fc2ee
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-10-15 21:13:24 -0600

    exports refactored UI and animations

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,16 +3,23 @@ export { ApiOfflineNotice } from './_MainAppLayout/ApiOfflineNotice'
 export { Footer } from './_MainAppLayout/Footer';
 export { HorizonSiteTitle } from './_MainAppLayout/HorizonSiteTitle';
 export { LoadingBar } from './_MainAppLayout/LoadingBar'
-export { MainQuerySessionInput } from './_MainAppLayout/MainQuerySessionInput'
 export { SessionSettings } from './_MainAppLayout/SessionSettings'
 export { VerticalSiteTitle } from './_MainAppLayout/VerticalSiteTitle';
 
 //ActiveQueryLayout
 export { ActiveQueryControls } from './_ActiveQueryLayout/ActiveQueryControls'
 export { EdgeDetails } from './_ActiveQueryLayout/EdgeDetails'
-export { HoveredVertexDetails } from './_ActiveQueryLayout/HoveredVertexDetails'
 export { RelatedEdgesDetails } from './_ActiveQueryLayout/RelatedEdgesDetails'
 export { VerTextDetails } from './_ActiveQueryLayout/VerTextDetails'
 
+//UI COMPONENTS:
+export { HoveredVertexDetailsDisplay } from './ui/HoveredVertexDetailsDisplay'
+export { InitializeQuerySessionInputMain } from './ui/InitializeQuerySessionInputMain'
+
+//ANIMATION FUNCTIONS:
+export { errorShakeInvalidElement } from './animations/ErrorShakeInvalidElement'
+export { toggleElementOpacity } from './animations/ToggleElementOpacity'
+export { fadeElementAndRemoveDisplay } from './animations/FadeElementAndRemoveDisplay'
+
 //debug menu
-export { SketchDataDebugSummary } from './debug/_SketchDataDebugSummary'
+export { SketchDataDebugSummary } from './debug/SketchDataDebugSummary'
