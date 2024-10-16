0dcc5a70927aeeec5f0d47a8e5d4020aaa2f7a32
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-10-15 22:42:32 -0600

    remove old to new files or refactor

--- src/components/_MainAppLayout/VerticalSiteTitle.tsx
+++
@@ -1,8 +0,0 @@
-import './VerticalSiteTitleStyle.css';
-import React from 'react';
-
-export const VerticalSiteTitle: React.FC = () => {
-  return (
-    <h1 id='vertical-site-title'>wikiverse</h1>
-  );
-};
