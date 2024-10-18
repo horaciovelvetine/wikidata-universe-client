ee521aeb0622a87bff18f3f1fca591763fdb46cf
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-10-17 21:45:46 -0600

    remove unused methods

--- src/models/Edge.ts
+++ src/models/Edge.ts
@@ -1,4 +1,3 @@
-import { SketchData } from "../interfaces";
 import { Vertex } from "./Vertex";
 
 export enum EDGE_DIR { INCOMING, OUTGOING, PARALLEL }
@@ -8,7 +7,6 @@ export interface iEdge {
   tgtId: string | null;
   propertyId: string;
   label: string | null;
-  type: string;
 }
 
 export class Edge implements iEdge {
@@ -16,29 +14,11 @@ export class Edge implements iEdge {
   tgtId: string | null;
   propertyId: string;
   label: string | null;
-  type: string;
   constructor(edgeData: iEdge) {
     this.srcId = edgeData.srcId;
     this.tgtId = edgeData.tgtId;
     this.propertyId = edgeData.propertyId;
     this.label = edgeData.label;
-    this.type = edgeData.type;
-  }
-
-  getVertexEndpoints(data: SketchData) {
-    // const endPoints = data.vertices.filter(vertexData =>
-    //   (vertexData.id == this.srcId || vertexData.id == this.tgtId)
-    // )
-    // if (this.srcId == endPoints[0].id) {
-    //   return { src: new Vertex(endPoints[0]), tgt: new Vertex(endPoints[1]) }
-    // }
-    // return { src: new Vertex(endPoints[1]), tgt: new Vertex(endPoints[0]) }
-  }
-
-  getPropertyDetails(data: SketchData) {
-    // const property: iProperty | undefined = data.properties.find(prop => this.propertyId == prop.id);
-
-    // return property ? property : new Property({ id: "def", label: "def", description: "def" })
   }
 
   getEdgeDirection(currentlySelectedVertex: Vertex) {
