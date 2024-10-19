d186fe4cdfc45fece5280afb5c62a2447b9321f2
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-10-18 19:00:05 -0600

    reflects changes for ignoring activeQuery, instead on screen for selected.... seems obvious in hindsight

--- src/components/lib/SelectedVertexDetails.tsx
+++ src/components/lib/SelectedVertexDetails.tsx
@@ -1,5 +1,5 @@
 import './SelectedVertexDetails.css'
-import { Vertex as VertexIcon, VertexSel } from '../../assets/icons'
+import { VertexSel } from '../../assets/icons'
 
 import { createRef, FC, useEffect } from 'react';
 import { SessionSettingsState } from '../../interfaces';
@@ -14,56 +14,30 @@ const prfx = (sufx: string) => {
   return 'selected-vertex-details-' + sufx;
 }
 
-const toggleSelectedVertexIcon = (noSelIcon: HTMLElement, selIcon: HTMLElement, isOpen: boolean, duration: number = 235) => {
-  const transition = `opacity ${duration}ms linear`;
-  noSelIcon.style.transition = transition;
-  selIcon.style.transition = transition;
-  if (isOpen) {
-    selIcon.style.opacity = '1';
-    noSelIcon.style.opacity = '0';
-  } else {
-    selIcon.style.opacity = '0';
-    noSelIcon.style.opacity = '1';
-  }
-}
-
-export const SelectedVertexDetails: FC<SelectedVertexDetailsProps> = ({ sessionSettingsState, selectedVertex }) => {
-  const { activeQuerySession } = sessionSettingsState;
+export const SelectedVertexDetails: FC<SelectedVertexDetailsProps> = ({ selectedVertex }) => {
   const displayRef = createRef<HTMLDivElement>();
-  const noneSelIconRef = createRef<HTMLImageElement>();
   const selIconRef = createRef<HTMLImageElement>();
   const textContRef = createRef<HTMLDivElement>();
 
   useEffect(() => {
-    if (!selectedVertex) {
-      toggleSelectedVertexIcon(noneSelIconRef.current!, selIconRef.current!, false)
+    if (!displayRef.current) return;
+    const transition = 'transform 200ms cubic-bezier(0.4, 0.0, 0.2, 1)';
+    displayRef.current.style.transition = transition;
+    if (selectedVertex == null) {
+      displayRef.current.style.transform = 'translateY(100%)';
     } else {
-      toggleSelectedVertexIcon(noneSelIconRef.current!, selIconRef.current!, true)
+      displayRef.current.style.transform = 'translateY(0)';
     }
   }, [selectedVertex])
-
-  useEffect(() => {
-    if (!displayRef.current) return;
-    // The scoot the bitch side ways protocol...
-    const transition = 'transform 500ms cubic-bezier(0.4, 0.0, 0.2, 1)';
-    displayRef.current.style.transition = transition;
-    if (activeQuerySession) {
-      displayRef.current.style.transform = 'translateX(0)';
-    } else {
-      displayRef.current.style.transform = 'translateX(-100%)';
-    }
-
-  }, [activeQuerySession])
 
   const label = selectedVertex ? (selectedVertex.label || selectedVertex.id) : '';
   const coords = selectedVertex ? selectedVertex.coordsStr() : '';
   const desc = selectedVertex ? selectedVertex.description : '';
 
   return (
-    <div id={prfx('display')} ref={displayRef} onWheel={(e) => { e.stopPropagation() }}>
+    <div id={prfx('display')} ref={displayRef} onWheel={(e) => { e.stopPropagation() }} onClick={(e => e.stopPropagation)}>
       <div id={prfx('icon-cont')}>
         <img id={prfx('selected-icon')} src={VertexSel} ref={selIconRef} />
-        <img id={prfx('none-selected-icon')} src={VertexIcon} ref={noneSelIconRef} />
       </div>
       <div id={prfx('text-cont')} ref={textContRef}>
         <a id={prfx('label-link')} href={selectedVertex?.url()} target='_blank'>
