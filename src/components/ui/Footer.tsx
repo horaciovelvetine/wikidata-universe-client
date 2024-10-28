9593ff6abbdd6abbbd0cb93783348e216c586d24
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-10-27 22:22:17 -0600

    footer can activate the about

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,23 +1,42 @@
+import { getAboutData, RequestResponse } from '../../api';
 import './Footer.css';
-import { FC } from 'react';
+import { Dispatch, FC, SetStateAction } from 'react';
 
-export const Footer: FC = () => {
+interface FooterProps {
+  setShowAboutSketch: Dispatch<SetStateAction<boolean>>;
+  setInitSketchAPIRes: Dispatch<SetStateAction<RequestResponse | null>>
+}
+
+export const Footer: FC<FooterProps> = ({ setShowAboutSketch, setInitSketchAPIRes }) => {
   const bl = "_blank";
   const githubUrl = "https://github.com/horaciovelvetine";
   const frontendUrl = githubUrl + "/wikidata-universe-client";
   const backendUrl = githubUrl + "/wikidata-universe-api";
-  const learnMoreUrl = githubUrl + "/ForceDrawnGraphs";
+
+  const handleAboutClick = async () => {
+    let prevState
+    setShowAboutSketch(prev => {
+      prevState = prev;
+      return !prev
+    })
+
+    if (!prevState) {
+      await getAboutData().then(res => {
+        setInitSketchAPIRes(res);
+      })
+    }
+  }
 
   return (
     <footer>
       <div id='container'>
         <ul id='list'>
-          <li id='ftr-item'>github{" "}
+          <li id='ftr-item'>
             <a href={frontendUrl} target={bl}>frontend</a>{" "}/{" "}
             <a href={backendUrl} target={bl}>backend</a>
           </li>
           <li id='ftr-item'>©2024 by <a href={githubUrl} target={bl}>@horaciovelvetine</a></li>
-          <li id='ftr-item'>about <a href={learnMoreUrl} target={bl}>wikiverse</a></li>
+          <li id='ftr-item'>about <a onClick={handleAboutClick}>wikiverse</a></li>
         </ul>
       </div>
     </footer>
