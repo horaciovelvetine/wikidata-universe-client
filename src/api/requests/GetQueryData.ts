2b9d5958d04f91366042e666b16214bdc6e66505
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-11-02 14:16:11 -0600

    included details in log

--- src/api/requests/GetQueryData.ts
+++ src/api/requests/GetQueryData.ts
@@ -4,7 +4,7 @@ import { RequestResponse, apiURL } from "..";
 export async function getQueryData(query: string): Promise<RequestResponse> {
   return await axios.get(apiURL('query-data'), { params: { query } })
     .then(res => {
-      console.log("getQueryData()", res.data)
+      console.log(`getQueryData(${query} : query )`, res.data)
       return { status: res.status, data: res.data };
     });
 }
