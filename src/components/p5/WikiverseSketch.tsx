c5bb49a00e0957ce183cd84061e7a220fb59758b
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-11-07 19:08:05 -0700

    update to match thinner state needed to pass to wikiVerseSketch matching changes in new AboutSketch stuff

--- src/components/p5/WikiverseSketch.tsx
+++ src/components/p5/WikiverseSketch.tsx
@@ -1,7 +1,7 @@
-import { Dispatch, FC, SetStateAction } from 'react';
+import { FC } from 'react';
 import { ReactP5Wrapper, Sketch } from '@p5-wrapper/react';
 
-import { SketchManager, Vertex } from '../../models';
+import { SketchManager } from '../../models';
 import { RequestResponse } from '../../api';
 import { MainAppLayoutState } from '../../app/MainAppLayoutState';
 
@@ -9,16 +9,13 @@ import { MainAppLayoutState } from '../../app/MainAppLayoutState';
 interface WikiverseProps {
   mainAppLayoutState: MainAppLayoutState;
   initSketchAPIRes: RequestResponse | null;
-  setSelectedVertex: Dispatch<SetStateAction<Vertex | null>>;
-  setHoveredVertex: Dispatch<SetStateAction<Vertex | null>>;
-  setP5SketchRef: Dispatch<SetStateAction<SketchManager | null>>;
 }
 
 //! { note } - Y axis is reversed of natural expectation in P5.js
-export const WikiverseSketch: FC<WikiverseProps> = ({ mainAppLayoutState, initSketchAPIRes, setSelectedVertex, setHoveredVertex, setP5SketchRef }) => {
+export const WikiverseSketch: FC<WikiverseProps> = ({ mainAppLayoutState, initSketchAPIRes }) => {
 
   const sketch: Sketch = (p5) => { //SketchManager contains a lionshare of the p5 sketch specific details
-    const SK = new SketchManager({ p5, initSketchAPIRes, mainAppLayoutState, setSelectedVertex, setHoveredVertex, setSketchRef: setP5SketchRef, isAboutSketch: false })
+    const SK = new SketchManager({ p5, initSketchAPIRes, mainAppLayoutState, isAboutSketch: false })
 
     //*/=> SETUP
     p5.preload = () => { SK.preloadFont() }
