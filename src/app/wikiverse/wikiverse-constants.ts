6f9b779e971472500dca87b127089654abef4590
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-02-17 00:35:07 -0700

    add homepage constant for Wikidata URL

--- src/app/wikiverse/wikiverse-constants.ts
+++ src/app/wikiverse/wikiverse-constants.ts
@@ -3,3 +3,5 @@ export const GITHUB_URL = "https://github.com/horaciovelvetine";
 export const FRONTEND_URL = GITHUB_URL + "/wikidata-universe-client";
 export const BACKEND_URL = GITHUB_URL + "/wikidata-universe-api";
 export const MAILTO_URL = "mailto:horaciovelvetine@gmail.com";
+export const WIKIDATA_HOMEPAGE =
+  "https://www.wikidata.org/wiki/Wikidata:Main_Page";
