071cbc4cf71459f05098f2f4c097201154ddd75c
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-02-17 00:41:45 -0700

    update container name follow

--- src/utils/get-main-disp-dimensions.ts
+++ src/utils/get-main-disp-dimensions.ts
@@ -1,14 +1,16 @@
-import { Dimensions } from "../types";
+import { defaultDimensions, Dimensions } from "../types";
 
 /**
  * @method getMainDispDimensions() - grabs the named container where the sketch is being drawn to let the sketch (& other dependant elements and functionality) how much space it has in PX and as a useable object (Dimensions)
  */
 export function getMainDispDimensions(): Dimensions {
-  const container = window.document.getElementById("main-display");
+  const container = window.document.getElementById(
+    "wikiverse-app-main-display"
+  );
   return container
-    ? new Dimensions({
+    ? {
         width: container.clientWidth,
         height: container.clientHeight,
-      })
-    : new Dimensions();
+      }
+    : defaultDimensions();
 }
