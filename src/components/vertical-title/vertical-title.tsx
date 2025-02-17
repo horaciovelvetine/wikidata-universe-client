f4afaef0c524f4fb75baf8a7a4a630c71b3a139f
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-02-17 00:47:32 -0700

    adds context comment and uses new hook for id and styling

--- src/components/vertical-title/vertical-title.tsx
+++ src/components/vertical-title/vertical-title.tsx
@@ -1,12 +1,17 @@
-import "./vertical-title.css";
-
 /**
- * For larger screens//positioned in grid position 4
+ * A Vertically run text title which appears in the #4 grid spot (left middle) on
+ * larger screens to make the grayspace a bit more interesting for widescreens.
+ * It uses a custom hook `useComponentID` to generate unique IDs for the elements.
+ *
+ * @returns A JSX element containing a div with a title.
  */
-const ID = (sufx: string) => `vertical-title-${sufx}`;
+import "./vertical-title.css";
+import { useComponentID } from "../../hooks";
+
 export const VerticalTitle = () => {
+  const { ID } = useComponentID("vertical-title");
   return (
-    <div id={"cont"}>
+    <div id={ID("cont")}>
       <h1 id={ID("text")}>wikiverse</h1>
     </div>
   );
