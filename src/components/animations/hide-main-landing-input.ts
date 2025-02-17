6c6091d754796c0521da00ee5710ce2ad528f3f2
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-02-17 00:44:07 -0700

    remove round 1 animations ongoing, move main landing to own named component outside of sketch HUD as own feature for landing page input

--- src/components/animations/hide-main-landing-input.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @method hideMainLandingInput() - hide the main input overlay after a successful response from the API signaling the start of exploring a new Query Sketch by the user.
- */
-export function hideMainLandingInput(container: HTMLDivElement) {
-  container.style.opacity = "0";
-  setTimeout(() => {
-    container.style.display = "none";
-  }, 235);
-}
---
+++ src/components/landing-page-input/landing-page-input.tsx
@@ -0,0 +1,94 @@
+import "./landing-page-input.css";
+import {
+  createRef,
+  Dispatch,
+  FormEvent,
+  SetStateAction,
+  useState,
+} from "react";
+
+import { Search, SearchDngr } from "../../assets/icons";
+
+import { WikiverseServiceResponse } from "../../types";
+import { useWikiverseService } from "../../providers";
+import { useComponentID } from "../../hooks";
+import { WIKIDATA_HOMEPAGE } from "../../app";
+
+interface LPInputProps {
+  sketchData: WikiverseServiceResponse | null;
+  setSketchData: Dispatch<SetStateAction<WikiverseServiceResponse | null>>;
+}
+
+/**
+ * The primary landing UI for the application which prompts the user for their search.
+ *
+ * @param {WikiverseServiceResponse | null} props.sketchData - used to determine the visibility of this @component
+ * @param {setSketchData} props.setSketchData - setter used to initialize a new sketch and begin the apps primary functionality.
+ */
+export const LandingPageInput = ({
+  sketchData,
+  setSketchData,
+}: LPInputProps) => {
+  const { ID } = useComponentID("main-landing");
+  const { isOnline, requestError, getNewQueryData } = useWikiverseService();
+  const [isErrored, setIsErrored] = useState(requestError);
+  const inputRef = createRef<HTMLInputElement>();
+
+  const handleSearchSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    const newQuery = inputRef.current?.value || "";
+    if (newQuery === "") {
+      // no input- provided set cycle local err state
+      setIsErrored(true);
+      setTimeout(() => {
+        setIsErrored(false);
+      }, 820);
+      return;
+    }
+    const data = await getNewQueryData(newQuery);
+
+    //TODO: cleanup and setup new sketch
+    setSketchData(data);
+  };
+
+  return (
+    <div
+      id={ID("container")}
+      className={!sketchData && isOnline ? "on-screen" : ""}
+    >
+      <h1 id={ID("title")}>
+        Explore{" "}
+        <a id={ID("wikidata-link")} href={WIKIDATA_HOMEPAGE} target="_blank">
+          Wikipedia
+        </a>{" "}
+        in 3D
+      </h1>
+      <form id={ID("form")} onSubmit={handleSearchSubmit}>
+        <input
+          id={ID("input")}
+          type="text"
+          placeholder="Search..."
+          autoFocus={true}
+          ref={inputRef}
+          className={isErrored ? "error-animated" : ""}
+        />
+        <button
+          id={ID("search-submit")}
+          type="submit"
+          className={isErrored ? "error-animated" : ""}
+        >
+          <img
+            id={ID("search-icon-danger")}
+            src={SearchDngr}
+            className={isErrored ? "error-animated" : ""}
+          />
+          <img
+            id={ID("search-icon")}
+            src={Search}
+            className={isErrored ? "error-animated" : ""}
+          />
+        </button>
+      </form>
+    </div>
+  );
+};
