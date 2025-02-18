4f12eeee279919b931177191f7e9ed182f8700d1
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-02-17 19:57:24 -0700

    add comments and links to used lib on github, fixes missing callback preventing re-render for new sketchData fixes re-search hangup

--- src/components/sketch-container/sketch-container/sketch-container.tsx
+++ src/components/sketch-container/sketch-container/sketch-container.tsx
@@ -11,19 +11,30 @@ export interface SketchProps {
   isTutorialSketch: boolean;
 }
 
+/**
+ * Main container element for the @see P5Sketch which draws the primary visuals for the application. Prevents rendering when there
+ * is no data and selectively renders the correct sub-type of Sketch based on what the client selects.
+ *
+ * @param {WikiverseServiceResponse | null} props.sketchData - can be null to return empty default element, otherwise initial data used to build a sketch
+ * @param {SetStateAction} props.setSketchRef - setter to allow react visibility into the active sketch once it is created
+ * @param {boolean} props.isTutorialSketch - used to select the sub-type of @see P5Sketch instead using the @see TutorialSketch
+ */
 export const SketchContainer = ({
   sketchData,
   setSketchRef,
   isTutorialSketch,
 }: SketchProps): JSX.Element => {
-  const Sketch = () => {
-    return isTutorialSketch ? (
-      <SketchTutorial {...{ sketchData, setSketchRef, isTutorialSketch }} />
+  const props = { sketchData, setSketchRef, isTutorialSketch };
+
+  // Defaults to the empty el when there is no sketchData present
+  // Else checks if this is meant to be a Tutorial or not
+  return sketchData ? (
+    isTutorialSketch ? (
+      <SketchTutorial {...props} />
     ) : (
-      <WikiverseSketch
-        {...{ sketchData, setSketchRef, isTutorialSketch }}
-      />
-    );
-  };
-  return !sketchData ? <></> : Sketch();
+      <WikiverseSketch {...props} />
+    )
+  ) : (
+    <></>
+  );
 };
