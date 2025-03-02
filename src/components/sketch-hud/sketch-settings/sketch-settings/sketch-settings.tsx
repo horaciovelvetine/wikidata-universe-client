9b42b0064235e9524b0353061ae1b4a0b72dffa4
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-03-02 11:56:24 -0700

    settings refacor adds comments and needed context notes and use, now using provider for unfocus click

--- src/components/sketch-hud/sketch-settings/sketch-settings/sketch-settings.tsx
+++ src/components/sketch-hud/sketch-settings/sketch-settings/sketch-settings.tsx
@@ -1,11 +1,10 @@
 import "./sketch-settings.css";
-import { createRef, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Settings } from "../../../../assets/icons";
 
-// TODO - remove animation
-import { showHideSettingsMenu } from "../../../animations/show-hide-settings-menu";
-import { P5Sketch } from "../../../../types";
-import { UnfocusClickToCloseWrapper } from "../hooks/use-unfocus-click-to-close-wrapper";
+import { P5Sketch, SketchRefProps } from "../../../../types";
+import { useComponentID } from "../../../../hooks";
+import { UnfocusedClickBoundaryProvider } from "../../../../providers";
 
 // Sub-Components
 import { OnScreenSettings } from "../on-screen-settings/on-screen-settings";
@@ -13,33 +12,50 @@ import { MouseSettings } from "../mouse-settings/mouse-settings";
 import { BehaviorSettings } from "../behavior-settings/behavior-settings";
 import { LayoutSettings } from "../layout-settings/layout-settings";
 
-interface SketchSettingsProps {
-  sketchRef: P5Sketch;
-}
-
-const ID = (sufx: string) => `sketch-settings-${sufx}`;
-
-export const SketchSettings = ({ sketchRef }: SketchSettingsProps) => {
+/**
+ * Component contains a variety of settings sub-components which allow the user to interact with and
+ * change settings and behaviors pertaining to the currently active {@link P5Sketch} instance.
+ *
+ * @component
+ * @param {SketchRefProps} props - a reference to the current sketch instance
+ *
+ * @returns {JSX.Element} SketchSettings component.
+ *
+ * @remarks
+ * Serves as a larger container for both the <img/> icon and #sketch-settings-menu-display <div/>.
+ * The icon is always visible in the upper right hand corner of the {@link SketchHUD} (enclosing container),
+ * while the #menu-display is added and removed from screen.
+ *
+ * Individual settings UI sub-components are:
+ * - {@link BehaviorSettings} - modifies behaviors within the P5Sketch instance itself
+ * - {@link LayoutSettings} - modify how Vertices are positioned inside the 'Wikiverse'
+ * - {@link MouseSettings} - modify user mouse sensitivity
+ * - {@link OnScreenSettings} - modify how or what is displaying inside the sketch
+ *
+ * @hook
+ * - UnfocusClickToClose - provider wraps settings to allow clicks outside the display to close the menu
+ * - useState - manage visibility state of the #sketch-settings-menu-display
+ */
+export const SketchSettings = ({ sketchRef }: SketchRefProps): JSX.Element => {
+  const { ID } = useComponentID("sketch-settings");
   const [showSettingsMenu, setShowSettingsMenu] = useState(
     sketchRef.state.showSketchSettings()
   );
-
-  const menuDisplayRef = createRef<HTMLDivElement>();
 
   useEffect(() => {
     // tell sketch how to let React know settings menu is open...
     sketchRef.state.addshowSketchsettingsSubscriber(setShowSettingsMenu);
   });
 
-  useEffect(() => {
-    showHideSettingsMenu(menuDisplayRef, showSettingsMenu);
-  }, [showSettingsMenu, menuDisplayRef]);
-
-  const handleGearIconClick = () => {
+  const handleToggleMenuShowIconClick = (
+    e: React.MouseEvent<HTMLImageElement>
+  ) => {
+    e.stopPropagation();
     sketchRef.state.toggleShowSketchSettings();
   };
 
   const handleUnfocusClick = () => {
+    // if the menu is is currently visible, and unfocused click should close the settings menu
     if (showSettingsMenu) {
       sketchRef.state.toggleShowSketchSettings();
     }
@@ -47,18 +63,20 @@ export const SketchSettings = ({ sketchRef }: SketchSettingsProps) => {
 
   return (
     <div id={ID("container")}>
-      <UnfocusClickToCloseWrapper onOutsideClickCallback={handleUnfocusClick}>
+      <UnfocusedClickBoundaryProvider
+        unfocusedClickCallback={handleUnfocusClick}
+      >
         <div id={ID("icon-container")}>
-          {/* <img id={ID('refresh-icon')} src={Fetch} onClick={handleRefreshClick} /> */}
           <img
             id={ID("gear-icon")}
             src={Settings}
-            onClick={handleGearIconClick}
+            onClick={e => handleToggleMenuShowIconClick(e)}
           />
         </div>
+
         <div
           id={ID("menu-display")}
-          ref={menuDisplayRef}
+          className={showSettingsMenu ? "on-screen" : "hidden"}
           onWheel={e => e.stopPropagation()}
           onClick={e => e.stopPropagation()}
         >
@@ -72,7 +90,7 @@ export const SketchSettings = ({ sketchRef }: SketchSettingsProps) => {
             <LayoutSettings {...{ sketchRef }} />
           </div>
         </div>
-      </UnfocusClickToCloseWrapper>
+      </UnfocusedClickBoundaryProvider>
     </div>
   );
 };
